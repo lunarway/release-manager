35875b21c2c5ddfe69fd13a317e14afbe6b6c325
Kasper Nissen <kaspernissen@gmail.com>
2019-02-27 13:10:53 +0100

    add git flags for rm-spec-gen and verify that project is working

--- cmd/rm-spec-gen/command/init.go
+++ cmd/rm-spec-gen/command/init.go
@@ -1,21 +1,35 @@
 package command
 
 import (
-	"github.com/lunarway/release-manager/cmd/rm-spec-gen/pkg/init"
+	"fmt"
+
+	"github.com/lunarway/release-manager/internal/spec"
 	"github.com/spf13/cobra"
 )
 
 // NewCommand sets up the move command
 func initCommand() *cobra.Command {
-	c := &cobra.Command{
+	var git spec.Git
+
+	command := &cobra.Command{
 		Use:   "init",
 		Short: "",
 		Long:  "",
 		RunE: func(cmd *cobra.Command, args []string) error {
-
-			return init.Run()
+			fmt.Printf("Git: %s %s %s %s", git.Author, git.Committer, git.Message, git.SHA)
+			return nil
 		},
 	}
 
-	return c
+	// Init git data
+	command.Flags().StringVar(&git.Author, "git-author", "", "the commit author")
+	command.Flags().StringVar(&git.Message, "git-message", "", "the commit message")
+	command.Flags().StringVar(&git.Committer, "git-committer", "", "the commit committer")
+	command.Flags().StringVar(&git.SHA, "git-sha", "", "the commit sha")
+	command.MarkFlagRequired("git-author")
+	command.MarkFlagRequired("git-message")
+	command.MarkFlagRequired("git-committer")
+	command.MarkFlagRequired("git-sha")
+
+	return command
 }
--- cmd/rm-spec-gen/command/root.go
+++ cmd/rm-spec-gen/command/root.go
@@ -2,13 +2,8 @@ package command
 
 import "github.com/spf13/cobra"
 
-type Options struct {
-	RootPath string
-}
-
 // NewCommand returns a new instance of a rm-gen-spec command.
 func NewCommand() *cobra.Command {
-	var options Options
 	var command = &cobra.Command{
 		Use:   "rm-spec-gen",
 		Short: "rm-spec-gen json generate service build specifications",
@@ -18,7 +13,5 @@ func NewCommand() *cobra.Command {
 	}
 
 	command.AddCommand(initCommand())
-
-	command.PersistentFlags().StringVar(&options.RootPath, "root", ".", "Root from where builds and releases should be found.")
 	return command
 }
