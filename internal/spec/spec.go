309648a9fbaf5f336c0c363b60b58abbd3b46a91
Kasper Nissen <kaspernissen@gmail.com>
2019-02-28 09:58:00 +0100

    update internal spec rename Git to Repository

--- internal/spec/spec.go
+++ internal/spec/spec.go
@@ -7,30 +7,33 @@ import (
 )
 
 type Spec struct {
-	Git        Git        `json:"git,omitempty"`
-	CI         CI         `json:"ci,omitempty"`
-	Squad      string     `json:"squad,omitempty"`
-	Repository Repository `json:"repository,omitempty"`
-	Stages     []Stage    `json:"stages,omitempty"`
+	Application Repository
+	CI          CI         `json:"ci,omitempty"`
+	Squad       string     `json:"squad,omitempty"`
+	Repository  Repository `json:"repository,omitempty"`
+	Shuttle     Shuttle    `json:"shuttle,omitempty"`
+	Stages      []Stage    `json:"stages,omitempty"`
 }
 
-type Git struct {
+type Repository struct {
 	SHA       string `json:"sha,omitempty"`
 	Author    string `json:"author,omitempty"`
 	Committer string `json:"committer,omitempty"`
 	Message   string `json:"message,omitempty"`
+	Name      string `json:"name,omitempty"`
+	URL       string `json:"url,omitempty"`
+	Provider  string `json:"provider,omitempty"`
+}
+
+type Shuttle struct {
+	Plan           Repository `json:"plan,omitempty"`
+	ShuttleVersion string     `json:"shuttleVersion,omitempty"`
 }
 
 type CI struct {
 	JobURL string    `json:"jobUrl,omitempty"`
 	Start  time.Time `json:"start,omitempty"`
 	End    time.Time `json:"end,omitempty"`
-}
-
-type Repository struct {
-	Name     string `json:"name,omitempty"`
-	URL      string `json:"url,omitempty"`
-	Provider string `json:"provider,omitempty"`
 }
 
 type Stage struct {
