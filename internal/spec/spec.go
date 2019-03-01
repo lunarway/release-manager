cded1dd1710e9f665e1fd14def7321c883df50fc
Kasper Nissen <kaspernissen@gmail.com>
2019-03-01 13:52:24 +0100

    update spec with artifact id

--- internal/spec/spec.go
+++ internal/spec/spec.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Spec struct {
+	ID          string     `json:"id,omitempty"`
 	Application Repository `json:"application,omitempty"`
 	CI          CI         `json:"ci,omitempty"`
 	Squad       string     `json:"squad,omitempty"`
