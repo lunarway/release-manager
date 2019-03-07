324ba3badd3f65178f63cd44dd54187897615213
Bjørn <bjoern.soerensen@gmail.com>
2019-03-07 07:31:07 +0100

    Update cmd/hamctl/command/root.go
    Co-Authored-By: kaspernissen <kaspernissen@gmail.com>

--- cmd/hamctl/command/root.go
+++ cmd/hamctl/command/root.go
@@ -25,7 +25,7 @@ func NewCommand() *cobra.Command {
 	command.AddCommand(NewPromote(&options))
 
 	command.PersistentFlags().StringVar(&options.RootPath, "root", ".", "Root from where builds and releases should be found.")
-	command.PersistentFlags().StringVar(&options.gRPCAddress, "grpc-address", "localhost:7900", "the address of the gRPC release manager server")
+	command.PersistentFlags().StringVar(&options.gRPCAddress, "grpc-address", "localhost:7900", "address of the gRPC release manager server")
 	command.PersistentFlags().DurationVar(&options.gRPCTimeout, "grpc-timeout", (20 * time.Second), "gRPC timeout")
 	return command
 }
