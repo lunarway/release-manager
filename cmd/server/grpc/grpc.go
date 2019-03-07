b32a2c7b718c6af81e7c4bee0e4e56d9e91b23ba
Bjørn <bjoern.soerensen@gmail.com>
2019-03-07 07:46:30 +0100

    Update cmd/server/grpc/grpc.go
    Co-Authored-By: kaspernissen <kaspernissen@gmail.com>

--- cmd/server/grpc/grpc.go
+++ cmd/server/grpc/grpc.go
@@ -22,7 +22,7 @@ func NewServer(port int, configRepo, artifactFileName string) error {
 	grpcServer := grpcpkg.NewServer()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return errors.WithMessage(err, "listen on tcp port")
 	}
 	gengrpc.RegisterReleaseManagerServer(grpcServer, &gRPCHandlers{
 		ArtifactFileName: artifactFileName,
