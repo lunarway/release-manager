87fbba7762849466cc97d331f7b495ddaed1e7a9
Kasper Nissen <kni@lunarway.com>
2019-04-02 08:11:22 +0200

    add version command to artifact

--- cmd/artifact/main.go
+++ cmd/artifact/main.go
@@ -3,12 +3,28 @@ package main
 import (
 	"os"
 
+	"fmt"
+
 	"github.com/lunarway/release-manager/cmd/artifact/command"
+	"github.com/spf13/cobra"
+)
+
+var (
+	version = ""
 )
 
 func main() {
-	err := command.NewCommand().Execute()
+	c, err := command.NewCommand()
 	if err != nil {
 		os.Exit(1)
 	}
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: "prints the version number of artifact",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(version)
+		},
+	}
+	c.AddCommand(versionCmd)
+	c.Execute()
 }
