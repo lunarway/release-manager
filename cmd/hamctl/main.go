efa2ddbf65d9bfe725bd3410b43cefa2ef520583
Kasper Nissen <kni@lunarway.com>
2019-04-02 08:07:39 +0200

    add version to hamctl

--- cmd/hamctl/main.go
+++ cmd/hamctl/main.go
@@ -3,12 +3,28 @@ package main
 import (
 	"os"
 
+	"fmt"
+
 	"github.com/lunarway/release-manager/cmd/hamctl/command"
+	"github.com/spf13/cobra"
+)
+
+var (
+	version = ""
 )
 
 func main() {
-	err := command.NewCommand().Execute()
+	c, err := command.NewCommand()
 	if err != nil {
 		os.Exit(1)
 	}
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: "prints the version number of hamctl",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(version)
+		},
+	}
+	c.AddCommand(versionCmd)
+	c.Execute()
 }
