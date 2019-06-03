875d324d99fcc1b4ad05cacb0ec7ca02a64e0e86
Bjørn <bso@lunarway.com>
2019-06-03 09:16:13 +0200

    Unwrap pkg/errors and multierr errors (#82)
    We cannot know if underlying functions have wrapped or aggregated
    errors, but we want to assert on them for better user messages.
    
    This changes ensures to unwrap errors properly for assertions.

--- cmd/server/http/error.go
+++ cmd/server/http/error.go
@@ -7,6 +7,8 @@ import (
 
 	httpinternal "github.com/lunarway/release-manager/internal/http"
 	"github.com/lunarway/release-manager/internal/log"
+	"github.com/pkg/errors"
+	"go.uber.org/multierr"
 )
 
 func Error(w http.ResponseWriter, message string, statusCode int) {
@@ -39,4 +41,16 @@ func requiredFieldError(w http.ResponseWriter, field string) {
 
 func requiredQueryError(w http.ResponseWriter, field string) {
 	Error(w, fmt.Sprintf("query param %s required but was empty", field), http.StatusBadRequest)
+}
+
+// errorCause unwraps err from pkg/errors messages and if err contains a
+// multierr, it will return the last err, again unwrapped if wrapped.
+func errorCause(err error) error {
+	// get cause before and after multierr unwrap to handle wrapped multierrs and
+	// multierrs with wrapped errors
+	errs := multierr.Errors(errors.Cause(err))
+	if len(errs) == 0 {
+		return nil
+	}
+	return errors.Cause(errs[len(errs)-1])
 }
