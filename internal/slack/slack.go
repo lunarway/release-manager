a9e5636632d01c89d5e3d086865e29c3d9f4a6c1
Kasper Nissen <kni@lunarway.com>
2019-07-04 13:13:33 +0200

    Do not post the message in a codebox

--- internal/slack/slack.go
+++ internal/slack/slack.go
@@ -237,7 +237,7 @@ func (c *Client) NotifySlackPolicySucceeded(email, title, message string) error 
 	attachments := slack.MsgOptionAttachments(slack.Attachment{
 		Title:      title,
 		Color:      MsgColorGreen,
-		Text:       fmt.Sprintf("```%s```", message),
+		Text:       fmt.Sprintf("%s", message),
 		MarkdownIn: []string{"text", "fields"},
 	})
 
