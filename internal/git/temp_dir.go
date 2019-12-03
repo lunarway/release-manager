a18d5316d20ad72723c427b0d13c3ac33ed73b55
Bjørn Sørensen <bso@lunarway.com>
2019-12-03 06:43:31 +0100

    Use tags for details of operations
    This makes comparison of traces better as the operation is the same and the
    details are added for read ability and not operational difference.

--- internal/git/temp_dir.go
+++ internal/git/temp_dir.go
@@ -13,7 +13,8 @@ import (
 // The first return argument is the path. The second is a close function to
 // remove the path.
 func TempDir(ctx context.Context, tracer tracing.Tracer, prefix string) (string, func(context.Context), error) {
-	span, _ := tracer.FromCtxf(ctx, "create temp dir for '%s'", prefix)
+	span, _ := tracer.FromCtxf(ctx, "create temp dir")
+	span.SetTag("path_prefix", prefix)
 	defer span.Finish()
 	path, err := ioutil.TempDir("", prefix)
 	if err != nil {
