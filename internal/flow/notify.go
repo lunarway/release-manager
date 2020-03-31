10831ef3513f453825dbbe5c390d9f3c0c098474
Kasper Nissen <kni@lunar.app>
2020-03-31 14:19:41 +0200

    remove duplicate lw email conversion

--- internal/flow/notify.go
+++ internal/flow/notify.go
@@ -4,10 +4,7 @@ import (
 	"context"
 	"regexp"
 
-	"strings"
-
 	"github.com/lunarway/release-manager/internal/http"
-	"github.com/lunarway/release-manager/internal/log"
 	"github.com/pkg/errors"
 )
 
@@ -15,22 +12,12 @@ func (s *Service) NotifyCommitter(ctx context.Context, event *http.PodNotifyRequ
 	span, ctx := s.Tracer.FromCtx(ctx, "flow.NotifyCommitter")
 	defer span.Finish()
 	email := event.AuthorEmail
-	if !strings.Contains(email, "@lunar.app") {
-		//check UserMappings
-		lwEmail, ok := s.UserMappings[email]
-		if !ok {
-			log.WithContext(ctx).Errorf("%s is not a Lunar Way email and no mapping exist", email)
-			return errors.Errorf("%s is not a Lunar Way email and no mapping exist", email)
-		}
-		email = lwEmail
-	}
 	span, _ = s.Tracer.FromCtx(ctx, "post private slack message")
 	err := s.Slack.PostPrivateMessage(ctx, email, event)
 	span.Finish()
 	if err != nil {
 		return errors.WithMessage(err, "post private message")
 	}
-
 	return nil
 }
 
@@ -49,15 +36,7 @@ func (s *Service) NotifyFluxEvent(ctx context.Context, event *http.FluxNotifyReq
 			return errors.WithMessagef(err, "parse commit message '%s'", commit.Message)
 		}
 		email := commitMessage.GitAuthor
-		if !strings.Contains(email, "@lunar.app") {
-			//check UserMappings
-			lwEmail, ok := s.UserMappings[email]
-			if !ok {
-				log.WithContext(ctx).Errorf("%s is not a Lunar email and no mapping exist", email)
-				return errors.Errorf("%s is not a Lunar email and no mapping exist", email)
-			}
-			email = lwEmail
-		}
+
 		// event contains errors, extract and post specific error message
 		if len(event.Errors) > 0 {
 			for _, err := range event.Errors {
