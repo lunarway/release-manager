96b3a43ef6f8761f3e5da4d5650ad93cd89cb08c
Bjørn Hald Sørensen <bso@lunar.app>
2020-04-14 11:55:12 +0200

    Add publish confirm mode for AMQP deliveries
    This change set implements publish confirmation in the AMQP broker. It will
    ensure that the AMQP server confirms all publishes and that the Publish method
    does not return before this confirmation is received.
    
    The timeout between publishes can be configured with flag
    amqp-republish-timeouts and defaults to 1 second. During republishes contexts
    cancellation and shutdowns are respected and will result in context.Cancelled
    and broker.ErrBrokerClosed errors respectively.

--- internal/broker/amqp/publisher.go
+++ internal/broker/amqp/publisher.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/lunarway/release-manager/internal/broker"
 	"github.com/lunarway/release-manager/internal/log"
 	"github.com/lunarway/release-manager/internal/tracing"
 	"github.com/pkg/errors"
@@ -11,19 +12,38 @@ import (
 )
 
 type rawPublisher struct {
-	channel      *amqp.Channel
-	exchangeName string
-	routingKey   string
+	channel          *amqp.Channel
+	exchangeName     string
+	routingKey       string
+	republishTimeout time.Duration
+
+	// close is used to signal that the publisher should close and any resends
+	// should be stopped.
+	close chan struct{}
+	// closed is set to true
+	closed        bool
+	confirmations chan amqp.Confirmation
+	republishing  func(ctx context.Context, reason error)
 }
 
-func newPublisher(amqpConn *amqp.Connection, exchangeName string) (publisher, error) {
+func newPublisher(amqpConn *amqp.Connection, exchangeName string, republishTimeout time.Duration, republishing func(context.Context, error)) (*rawPublisher, error) {
 	channel, err := amqpConn.Channel()
 	if err != nil {
 		return nil, errors.WithMessage(err, "get channel")
 	}
+	err = channel.Confirm(false)
+	if err != nil {
+		return nil, errors.WithMessage(err, "enable confirm mode")
+	}
+	confirmations := make(chan amqp.Confirmation, 1)
+	channel.NotifyPublish(confirmations)
 	return &rawPublisher{
-		channel:      channel,
-		exchangeName: exchangeName,
+		channel:          channel,
+		exchangeName:     exchangeName,
+		close:            make(chan struct{}),
+		confirmations:    confirmations,
+		republishing:     republishing,
+		republishTimeout: republishTimeout,
 	}, nil
 }
 
@@ -35,16 +55,56 @@ func (p *rawPublisher) Publish(ctx context.Context, eventType, messageID string,
 		MessageId:     messageID,
 		CorrelationId: tracing.RequestIDFromContext(ctx),
 	}
-	// TODO: add publisher confirms to the channel and wait for acknowledgement
-	// before returning
-	err := p.channel.Publish(p.exchangeName, p.routingKey, false, false, pub)
-	if err != nil {
-		return err
+	for {
+		err := p.channel.Publish(p.exchangeName, p.routingKey, false, false, pub)
+		if err != nil {
+			// TODO: handle lost channels here. If the channel gets closed, for some
+			// reason, we will keep retrying
+
+			// retry the publish after a timeout or stop on context cancellation or if
+			// the publisher is closed
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-p.close:
+				return broker.ErrBrokerClosed
+			case <-time.After(p.republishTimeout):
+				p.republishing(ctx, err)
+				continue
+			}
+		}
+		// wait for confirmation of the publish but respect context cancellation and
+		// if the publisher is closed
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-p.close:
+			return broker.ErrBrokerClosed
+		case confirm, ok := <-p.confirmations:
+			// the confirmations channel can be closed if the amqp channel is closed
+			// unexpectedly and this the read will be possible. We do not expect this
+			// case nor handle it in the publisher so this is just a fail guard until
+			// prober handling is in place. Note that this will only be triggered if
+			// the AMQP channel is closed, not the connection. If the connection is
+			// closed the whole publisher will be replaced.
+			if !ok {
+				return errors.New("confirmation channel closed")
+			}
+			if confirm.Ack {
+				return nil
+			}
+		case <-time.After(p.republishTimeout):
+			p.republishing(ctx, errors.New("published timed out"))
+		}
 	}
-	return nil
 }
 
 func (p *rawPublisher) Close() error {
+	if p.closed {
+		return nil
+	}
+	close(p.close)
+	p.closed = true
 	err := p.channel.Close()
 	if err != nil {
 		return errors.WithMessage(err, "close amqp channel")
