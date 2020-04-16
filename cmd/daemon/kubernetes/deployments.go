cceb37f1e5266457fa630393af557bb9fa789505
Kasper Nissen <kni@lunar.app>
2020-04-16 11:55:06 +0200

    Update cmd/daemon/kubernetes/deployments.go
    Co-Authored-By: Bjørn <bso@lunar.app>

--- cmd/daemon/kubernetes/deployments.go
+++ cmd/daemon/kubernetes/deployments.go
@@ -121,7 +121,7 @@ func isDeploymentCorrectlyAnnotated(deploy *appsv1.Deployment) bool {
 
 	// Just discard the event if there's no artifact id
 	if deploy.Annotations["lunarway.com/artifact-id"] == "" {
-		log.Errorf("artifact-id missing in deployment: namespace '%s' pod '%s'", deploy.Namespace, deploy.Name)
+		log.Errorf("artifact-id missing in deployment: namespace '%s' name '%s'", deploy.Namespace, deploy.Name)
 		return false
 	}
 
