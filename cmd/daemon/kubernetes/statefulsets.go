0634577d55a42d7edb6824b64a296ddd09765251
Kasper Nissen <kni@lunar.app>
2020-04-19 20:04:12 +0200

    Add watch for statefulsets

---
+++ cmd/daemon/kubernetes/statefulsets.go
@@ -0,0 +1,112 @@
+package kubernetes
+
+import (
+	"context"
+
+	"github.com/prometheus/common/log"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+)
+
+func (c *Client) HandleNewStatefulSets(ctx context.Context) error {
+	watcher, err := c.clientset.AppsV1().StatefulSets("").Watch(ctx, metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+	for {
+		select {
+		case <-ctx.Done():
+			watcher.Stop()
+		case e, ok := <-watcher.ResultChan():
+			if !ok {
+				return ErrWatcherClosed
+			}
+			if e.Object == nil {
+				continue
+			}
+			if e.Type == watch.Deleted {
+				continue
+			}
+			ss, ok := e.Object.(*appsv1.StatefulSet)
+			if !ok {
+				continue
+			}
+
+			// Check if we have all the annotations we need for the release-daemon
+			if !isStatefulSetCorrectlyAnnotated(ss) {
+				continue
+			}
+
+			// Avoid reporting on pods that has been marked for termination
+			if isStefulSetMarkedForTermination(ss) {
+				continue
+			}
+
+			// Verify if the StatefulSet fulfills the criterias for a succesful release
+			ok = isStatefulSetSuccessful(ss)
+			if !ok {
+				continue
+			}
+
+			// In-order to minimize messages and only return events when new releases is detected, we add
+			// a new annotation to the DaemonSet. This annotations tells provides the daemon with some valuable
+			// information about the artifacts running.
+			// When we initially apply a DaemonSet the lunarway.com/artifact-id annotations SHOULD be set.
+			// Further the observed-artifact-id is an annotation managed by the daemon and will initially be "".
+			// In this state we annotate the DaemonSet with the current artifact-id as the observed.
+			// When we update a DaemonSet we also update the artifact-id, e.g. now observed and actual artifact id
+			// is different. In this case we want to notify, and update the observed with the current artifact id.
+			// This also eliminates messages when a pod is deleted. As the two annotations will be equal.
+			if ss.Annotations["lunarway.com/observed-artifact-id"] == ss.Annotations["lunarway.com/artifact-id"] {
+				continue
+			}
+
+			// Annotate the DaemonSet to be able to skip it next time
+			err = annotateStatefulSet(ctx, c.clientset, ss)
+			if err != nil {
+				log.Errorf("Unable to annotate StatefulSet: %v", err)
+				continue
+			}
+
+			log.Infof("Notify successful StatefulSet: %+v", ss)
+		}
+	}
+}
+
+// Avoid reporting on StatefulSets that has been marked for termination
+func isStefulSetMarkedForTermination(ss *appsv1.StatefulSet) bool {
+	return ss.DeletionTimestamp != nil
+}
+
+func isStatefulSetSuccessful(ss *appsv1.StatefulSet) bool {
+	return ss.Status.Replicas == ss.Status.ReadyReplicas &&
+		ss.Status.Replicas == ss.Status.CurrentReplicas &&
+		ss.Status.Replicas == ss.Status.UpdatedReplicas &&
+		ss.Status.ObservedGeneration >= ss.Generation
+}
+
+func isStatefulSetCorrectlyAnnotated(ss *appsv1.StatefulSet) bool {
+	if !(ss.Annotations["lunarway.com/controlled-by-release-manager"] == "true") {
+		return false
+	}
+	if ss.Annotations["lunarway.com/artifact-id"] == "" {
+		log.Errorf("artifact-id missing in statefulset: namespace '%s' name '%s'", ss.Namespace, ss.Name)
+		return false
+	}
+	if ss.Annotations["lunarway.com/author"] == "" {
+		log.Errorf("author missing in statefulset: namespace '%s' name '%s'", ss.Namespace, ss.Name)
+		return false
+	}
+	return true
+}
+
+func annotateStatefulSet(ctx context.Context, c *kubernetes.Clientset, ss *appsv1.StatefulSet) error {
+	ss.Annotations["lunarway.com/observed-artifact-id"] = ss.Annotations["lunarway.com/artifact-id"]
+	_, err := c.AppsV1().StatefulSets(ss.Namespace).Update(ctx, ss, metav1.UpdateOptions{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
