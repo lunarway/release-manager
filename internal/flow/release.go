lunarway/release-manager

commit 0a99b155c382e517b1785fdeb5b0f773dff632af

Ignore missing artifacts from existing release (#161)
When testing if the current released artifact is the same as a new release we
might hit an artifact.ErrFileNotFound error if there has never been a release to
the target environment.

This change set ensures that this error is ignored in these checks to avoid
effectively blocking releases to new environments.

It also adds a check to the release artifact flow that was missed in
aff012fd6a433664956876a5adfa640cfcb0a9ac (#150).

internal/flow/release.go
@@ -59,9 +59,11 @@ func (s *Service) ReleaseBranch(ctx context.Context, actor Actor, environment, s
 	}
 
 	// check that the artifact to be released is not already released in the
-	// environment
+	// environment. If there is no artifact released to the target environment an
+	// artifact.ErrFileNotFound error is returned. This is OK as the currentSpec
+	// will then be the default value and this its ID will be the empty string.
 	currentSpec, err := envSpec(sourceConfigRepoPath, s.ArtifactFileName, service, environment, namespace)
-	if err != nil {
+	if err != nil && errors.Cause(err) != artifact.ErrFileNotFound {
 		return "", errors.WithMessage(err, "get current released spec")
 	}
 	if currentSpec.ID == artifactSpec.ID {
@@ -262,6 +264,18 @@ func (s *Service) ReleaseArtifactID(ctx context.Context, actor Actor, environmen
 	namespace := sourceSpec.Namespace
 	if namespace == "" {
 		namespace = environment
+	}
+
+	// check that the artifact to be released is not already released in the
+	// environment. If there is no artifact released to the target environment an
+	// artifact.ErrFileNotFound error is returned. This is OK as the currentSpec
+	// will then be the default value and this its ID will be the empty string.
+	currentSpec, err := envSpec(sourceConfigRepoPath, s.ArtifactFileName, service, environment, namespace)
+	if err != nil && errors.Cause(err) != artifact.ErrFileNotFound {
+		return "", errors.WithMessage(err, "get current released spec")
+	}
+	if currentSpec.ID == sourceSpec.ID {
+		return "", ErrNothingToRelease
 	}
 
 	err = s.PublishReleaseArtifactID(ctx, ReleaseArtifactIDEvent{

internal/flow/promote.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"path"
 
+	"github.com/lunarway/release-manager/internal/artifact"
 	"github.com/lunarway/release-manager/internal/copy"
 	"github.com/lunarway/release-manager/internal/git"
 	"github.com/lunarway/release-manager/internal/log"
@@ -86,9 +87,11 @@ func (s *Service) Promote(ctx context.Context, actor Actor, environment, namespa
 		}
 
 		// check that the artifact to be released is not already released in the
-		// environment
+		// environment. If there is no artifact released to the target environment an
+		// artifact.ErrFileNotFound error is returned. This is OK as the currentSpec
+		// will then be the default value and this its ID will be the empty string.
 		currentSpec, err := envSpec(sourceConfigRepoPath, s.ArtifactFileName, service, environment, namespace)
-		if err != nil {
+		if err != nil && errors.Cause(err) != artifact.ErrFileNotFound {
 			return true, errors.WithMessage(err, "get current released spec")
 		}
 		if currentSpec.ID == sourceSpec.ID {

commit 8dc4ee03984329a6b0d1fd6151ca4a120f641814

Compare artifact releases correctly
When releasing a specific artifact the sync flow validates whether there will be
anything to release by comparing the released artifact id to the request one.
The comparison was done on the artifacts source Git sha instead of the HEAD SHA.
This is a bug introduced in 0a99b155c382e517b1785fdeb5b0f773dff632af (#161)
where the check was added but the origin of the environment spec was wrong.

When handling release.artifactId events the server did also not checkout the
hash of the artifact and thus release the latest artifact for the service
instead of the specified one. This bug was introduced during the async flow
change b1c9fe82f11e11cd2abb1e084ead74dc2f0a8e9c (#140) where the checkout was
only kept in the sync section of the flow.

internal/flow/release.go
@@ -270,7 +270,16 @@ func (s *Service) ReleaseArtifactID(ctx context.Context, actor Actor, environmen
 	// environment. If there is no artifact released to the target environment an
 	// artifact.ErrFileNotFound error is returned. This is OK as the currentSpec
 	// will then be the default value and this its ID will be the empty string.
-	currentSpec, err := envSpec(sourceConfigRepoPath, s.ArtifactFileName, service, environment, namespace)
+	destinationConfigRepoPath, closeDestinationSource, err := git.TempDirAsync(ctx, s.Tracer, "k8s-config-release-artifact-destination")
+	if err != nil {
+		return "", err
+	}
+	defer closeDestinationSource(ctx)
+	_, err = s.Git.Clone(ctx, destinationConfigRepoPath)
+	if err != nil {
+		return "", errors.WithMessagef(err, "clone into '%s'", sourceConfigRepoPath)
+	}
+	currentSpec, err := envSpec(destinationConfigRepoPath, s.ArtifactFileName, service, environment, namespace)
 	if err != nil && errors.Cause(err) != artifact.ErrFileNotFound {
 		return "", errors.WithMessage(err, "get current released spec")
 	}
@@ -304,7 +313,7 @@ func (s *Service) ExecReleaseArtifactID(ctx context.Context, event ReleaseArtifa
 		}
 		defer closeSource(ctx)
 
-		_, err = s.Git.Clone(ctx, sourceConfigRepoPath)
+		sourceRepo, err := s.Git.Clone(ctx, sourceConfigRepoPath)
 		if err != nil {
 			return true, errors.WithMessagef(err, "clone into '%s'", sourceConfigRepoPath)
 		}
@@ -326,6 +335,15 @@ func (s *Service) ExecReleaseArtifactID(ctx context.Context, event ReleaseArtifa
 		namespace := event.Namespace
 		actor := event.Actor
 		artifactID := event.ArtifactID
+
+		hash, err := s.Git.LocateArtifact(ctx, sourceRepo, artifactID)
+		if err != nil {
+			return true, errors.WithMessagef(err, "locate release '%s'", artifactID)
+		}
+		err = s.Git.Checkout(ctx, sourceConfigRepoPath, hash)
+		if err != nil {
+			return true, errors.WithMessagef(err, "checkout release hash '%s'", hash)
+		}
 
 		// release service to env from original release
 		sourcePath := srcPath(sourceConfigRepoPath, service, branch, environment)