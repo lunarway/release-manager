6e21154075a42f2ee365c84350993bc4f4c58268
Bjørn Hald Sørensen <bso@lunar.app>
2020-05-29 13:50:55 +0200

    Implement ArtifactPaths

--- internal/s3storage/artifactreadstorage.go
+++ internal/s3storage/artifactreadstorage.go
@@ -3,13 +3,19 @@ package s3storage
 import (
 	"context"
 	"fmt"
+	"os"
+	"path"
 	"sort"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/lunarway/release-manager/internal/artifact"
+	"github.com/lunarway/release-manager/internal/git"
+	"github.com/lunarway/release-manager/internal/log"
+	"github.com/lunarway/release-manager/internal/tracing"
 	"github.com/pkg/errors"
 )
 
@@ -32,30 +38,65 @@ func (f *Service) ArtifactSpecification(ctx context.Context, service string, art
 }
 
 func (f *Service) ArtifactPaths(ctx context.Context, service string, environment string, branch string, artifactID string) (specPath string, resourcesPath string, close func(context.Context), err error) {
-	// 	logger := log.WithContext(ctx)
+	logger := log.WithContext(ctx)
 
-	// 	sourceConfigRepoPath, closeSource, err := TempDirAsync(ctx, s.Tracer, "s3-artifact-paths")
-	// 	if err != nil {
-	// 		return "", "", nil, errors.WithMessage(err, "get temp dir")
-	// 	}
+	// FIXME:  we should move that out of package git
+	zipDestPath, closeSource, err := git.TempDirAsync(ctx, tracing.NewNoop(), "s3-artifact-paths")
+	if err != nil {
+		return "", "", nil, errors.WithMessage(err, "get temp dir")
+	}
+	defer closeSource(ctx)
+	zipDestPath = path.Join(zipDestPath, "artifact.zip")
+	logger.Debugf("Zip destination: %s", zipDestPath)
+	zipDest, err := os.Create(zipDestPath)
+	if err != nil {
+		return "", "", nil, errors.WithMessage(err, "get temp dir")
+	}
+	defer func() {
+		err := zipDest.Close()
+		if err != nil {
+			logger.Errorf("Failed to close zip destination file: %v", err)
+		}
+	}()
 
-	// 	out, err := svc.GetObject(&s3.GetObjectInput {
-	// 		Bucket: aws.String(f.bucketName),
-	// 		 Key: aws.String(getObjectKeyName(service)),
-	// })
-	// 	list, er= nil {
-	// 		return "", err
-	// 	}
+	key := getObjectKeyName(service, artifactID)
+	downloader := s3manager.NewDownloaderWithClient(f.s3client)
+	logger.Infof("Downloading object at key '%s'", key)
+	n, err := downloader.DownloadWithContext(ctx, zipDest, &s3.GetObjectInput{
+		Bucket: aws.String(f.bucketName),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return "", "", nil, errors.WithMessage(err, "download object")
+	}
+	logger.Infof("Downloaded %d bytes", n)
 
-	// 	sort.Sl
+	// FIXME:  we should move that out of package git
+	destPath, closeSource, err := git.TempDirAsync(ctx, tracing.NewNoop(), "s3-artifact-paths")
+	if err != nil {
+		return "", "", nil, errors.WithMessage(err, "get temp dir")
+	}
+	logger.Infof("Resources dest: %s", destPath)
 
-	// ice(list.Contents, func(i, j int) bool {
-	// 		return list.Contents[i].LastModified.After(*list.Contents[j].LastModified)
-	// 	})
+	files, err := unzipFile(zipDestPath, destPath)
+	if err != nil {
+		// manually close destination directory here as we must allow callers to
+		// access the directory and this cannot use defer
+		closeSource(ctx)
+		return "", "", nil, errors.WithMessage(err, "unzip file")
+	}
+	logger.Infof("Found files: %v", files)
 
-	// 	return *list.Contents[0].Key, nil
+	return getArtifactFilePath(files, "artifact.json"), path.Join(destPath, environment), closeSource, nil
+}
 
-	return "", "", nil, fmt.Errorf("artifact not found")
+func getArtifactFilePath(files []string, fileName string) string {
+	for _, file := range files {
+		if strings.HasSuffix(file, fileName) {
+			return file
+		}
+	}
+	return ""
 }
 
 func (f *Service) LatestArtifactSpecification(ctx context.Context, service string, branch string) (artifact.Spec, error) {
