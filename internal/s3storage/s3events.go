40f0a0684978d594e9e32c3a4bca16d1e906078e
Emil Ingerslev <eki@lunar.app>
2020-06-06 08:19:53 +0200

    Remove misplaced comments from s3events

--- internal/s3storage/s3events.go
+++ internal/s3storage/s3events.go
@@ -27,26 +27,26 @@ type S3EventRecord struct {
 }
 
 type S3EventRecordIdentity struct {
-	PrincipalId string `json:"principalId,omitempty"` //"AWS:AROAIVBPOFACJXCN6JNTG:eki@lunar.app"
+	PrincipalId string `json:"principalId,omitempty"`
 }
 
 type S3EventRecordData struct {
-	S3SchemaVersion string                  `json:"s3SchemaVersion,omitempty"` //"1.0",
-	ConfigurationId string                  `json:"configurationId,omitempty"` //"ZWU1OTJlYTAtOTBhMC00NzkwLWE1NTEtMWE2NGM0MTU5YWRl",
+	S3SchemaVersion string                  `json:"s3SchemaVersion,omitempty"`
+	ConfigurationId string                  `json:"configurationId,omitempty"`
 	Bucket          S3EventRecordDataBucket `json:"bucket,omitempty"`
 	Object          S3EventRecordDataObject `json:"object,omitempty"`
 }
 
 type S3EventRecordDataBucket struct {
-	Name          string                `json:"name,omitempty"` //"eki-lunar-artifacts",
+	Name          string                `json:"name,omitempty"`
 	OwnerIdentity S3EventRecordIdentity `json:"ownerIdentity,omitempty"`
-	Arn           string                `json:"arn,omitempty"` //"arn:aws:s3:::eki-lunar-artifacts"
+	Arn           string                `json:"arn,omitempty"`
 }
 
 type S3EventRecordDataObject struct {
-	Key       string `json:"key,omitempty"` //"test-service/master-20200604-1303",
+	Key       string `json:"key,omitempty"`
 	Size      int    `json:"size,omitempty"`
-	ETag      string `json:"eTag,omitempty"`      //"c12e30c98ebc8cffb28215a61651614e",
-	VersionID string `json:"versionId,omitempty"` // object version if bucket is versioning-enabled, otherwise null
-	Sequencer string `json:"sequencer,omitempty"` //"005ED8D51FB571C1E8"
+	ETag      string `json:"eTag,omitempty"`
+	VersionID string `json:"versionId,omitempty"`
+	Sequencer string `json:"sequencer,omitempty"`
 }
