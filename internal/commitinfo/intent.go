b5249c78e73900c89b9dcdc9c19c1e6fb66fae9f
Emil Ingerslev <eki@lunar.app>
2020-06-24 10:50:41 +0200

    Put commitinfo field names into const's

--- internal/commitinfo/intent.go
+++ internal/commitinfo/intent.go
@@ -4,14 +4,20 @@ import (
 	"github.com/lunarway/release-manager/internal/intent"
 )
 
+const (
+	FieldRollbackOfArtifactId    = "Rollback-of-artifact-id"
+	FieldReleaseOfBranch         = "Release-of-branch"
+	FieldPromotedFromEnvironment = "Promoted-from-environment"
+)
+
 func ParseIntent(cci ConventionalCommitInfo) intent.Intent {
 	switch cci.Field("Release-intent") {
 	case intent.TypeReleaseBranch:
-		return intent.NewReleaseBranch(cci.Field("Release-branch"))
+		return intent.NewReleaseBranch(cci.Field(FieldReleaseOfBranch))
 	case intent.TypePromote:
-		return intent.NewPromoteEnvironment(cci.Field("Release-environment"))
+		return intent.NewPromoteEnvironment(cci.Field(FieldPromotedFromEnvironment))
 	case intent.TypeRollback:
-		return intent.NewRollback(cci.Field("Release-rollback-of-artifact-id"))
+		return intent.NewRollback(cci.Field(FieldRollbackOfArtifactId))
 	case intent.TypeAutoRelease:
 		return intent.NewAutoRelease()
 	default:
@@ -23,11 +29,11 @@ func AddIntentToConventionalCommitInfo(intentObj intent.Intent, cci *Conventiona
 	cci.SetField("Release-intent", intentObj.Type)
 	switch intentObj.Type {
 	case intent.TypeReleaseBranch:
-		cci.SetField("Release-branch", intentObj.ReleaseBranch.Branch)
+		cci.SetField(FieldReleaseOfBranch, intentObj.ReleaseBranch.Branch)
 	case intent.TypePromote:
-		cci.SetField("Release-environment", intentObj.Promote.FromEnvironment)
+		cci.SetField(FieldPromotedFromEnvironment, intentObj.Promote.FromEnvironment)
 	case intent.TypeRollback:
-		cci.SetField("Release-rollback-of-artifact-id", intentObj.Rollback.PreviousArtifactID)
+		cci.SetField(FieldRollbackOfArtifactId, intentObj.Rollback.PreviousArtifactID)
 	case intent.TypeAutoRelease:
 		// nothing yet
 	}
