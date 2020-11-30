7c88539f3ee6c137f386f73fd52402e7aa98d545
Martin Anker Have <mah@lunar.app>
2020-11-30 14:58:26 +0100

    feature/Release Manager Url Environment Variable (#215)
    * Set http url default value based on os env variable
    
    * Updated push artifact command to also use default os env variables
    
    * Default url to environment variable
    
    * Fix wrong import
    
    * Set BaseUrl and authToken if flags were not set before each command
    
    * Remove whitespace to simplify pr

--- cmd/artifact/command/push.go
+++ cmd/artifact/command/push.go
@@ -3,6 +3,7 @@ package command
 import (
 	"context"
 	"fmt"
+	"os"
 	"path"
 
 	"github.com/lunarway/release-manager/internal/flow"
@@ -47,7 +48,7 @@ func pushCommand(options *Options) *cobra.Command {
 			return nil
 		},
 	}
-	command.Flags().StringVar(&releaseManagerClient.BaseURL, "http-base-url", "https://release-manager.dev.lunarway.com", "address of the http release manager server")
+	command.Flags().StringVar(&releaseManagerClient.BaseURL, "http-base-url", os.Getenv("ARTIFACT_URL"), "address of the http release manager server")
 	command.Flags().StringVar(&releaseManagerClient.Metadata.AuthToken, "http-auth-token", "", "auth token for the http service")
 
 	// errors are skipped here as the only case they can occour are if thee flag
