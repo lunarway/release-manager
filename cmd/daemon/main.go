da5c14538bf4baa4d5ef2993827d3553c86085de
Bjørn Sørensen <bso@lunar.app>
2020-12-21 10:44:18 +0100

    Extract version command setup
    Currently the version commands are attached to the root commands unlike any
    other of the commands. This is odd and unexpected.
    
    This change moves the version commands into their own files and attaches them to
    the root like every other command.
    
    Furhter more the make targets for building the Go binaries are updated to
    default to the current head git sha as version. This is mostly for testing but
    also ensures that any distribution of a locally built binary is trackable.

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/lunarway/release-manager/cmd/daemon/command"
 	"github.com/lunarway/release-manager/internal/log"
-	"github.com/spf13/cobra"
 )
 
 var (
@@ -14,20 +12,11 @@ var (
 )
 
 func main() {
-	c, err := command.DaemonCommand()
+	c, err := command.NewRoot(version)
 	if err != nil {
 		log.Errorf("Error: %v", err)
 		os.Exit(1)
 	}
-
-	versionCmd := &cobra.Command{
-		Use:   "version",
-		Short: "prints the version number of artifact",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println(version)
-		},
-	}
-	c.AddCommand(versionCmd)
 	err = c.Execute()
 	if err != nil {
 		log.Errorf("Error: %v", err)
