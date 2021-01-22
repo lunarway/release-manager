lunarway/release-manager

commit 73d80a895f35015e30975ef9b75fcaea14493926

Remove lunar specific things (#214)
* Remove lunar specific email from artifact cmd

* Make email used in internal slack generic

* Remove lunar from comments and log messages

* Change to hasSuffix check

* ⚠️ Make 'email-suffix' flag required

cmd/artifact/command/add.go
@@ -240,7 +240,7 @@ func setStage(s artifact.Spec, stage artifact.Stage) artifact.Spec {
 }
 
 func notifySlack(options *Options, text, color string) error {
-	client, err := slack.NewClient(options.SlackToken, options.UserMappings)
+	client, err := slack.NewClient(options.SlackToken, options.UserMappings, options.EmailSuffix)
 	if err != nil {
 		return err
 	}

cmd/artifact/command/failure.go
@@ -15,7 +15,7 @@ func failureCommand(options *Options) *cobra.Command {
 		Use:   "failure",
 		Short: "report failure in the pipeline",
 		RunE: func(c *cobra.Command, args []string) error {
-			client, err := slack.NewClient(options.SlackToken, options.UserMappings)
+			client, err := slack.NewClient(options.SlackToken, options.UserMappings, options.EmailSuffix)
 			if err != nil {
 				fmt.Printf("Error, not able to create Slack client in failure command: %v", err)
 				return nil

cmd/artifact/command/init.go
@@ -32,26 +32,26 @@ func initCommand(options *Options) *cobra.Command {
 			if err != nil {
 				return err
 			}
-			isLunarWayEmail := func(email string) bool {
-				return strings.Contains(email, "@lunar.app")
+			isCompanyEmail := func(email string) bool {
+				return strings.HasSuffix(email, options.EmailSuffix)
 			}
-			if !isLunarWayEmail(s.Application.AuthorEmail) {
-				lwEmail, ok := options.UserMappings[s.Application.AuthorEmail]
+			if !isCompanyEmail(s.Application.AuthorEmail) {
+				companyEmail, ok := options.UserMappings[s.Application.AuthorEmail]
 				if !ok {
 					// Don't break, just continue and use the provided email
 					fmt.Printf("user mappings for %s not found", s.Application.AuthorEmail)
 				} else {
-					s.Application.AuthorEmail = lwEmail
+					s.Application.AuthorEmail = companyEmail
 				}
 			}
 
-			if !isLunarWayEmail(s.Application.CommitterEmail) {
-				lwEmail, ok := options.UserMappings[s.Application.CommitterEmail]
+			if !isCompanyEmail(s.Application.CommitterEmail) {
+				companyEmail, ok := options.UserMappings[s.Application.CommitterEmail]
 				if !ok {
 					// Don't break, just continue and use the provided email
 					fmt.Printf("user mappings for %s not found", s.Application.CommitterEmail)
 				} else {
-					s.Application.CommitterEmail = lwEmail
+					s.Application.CommitterEmail = companyEmail
 				}
 			}
 
@@ -71,7 +71,7 @@ func initCommand(options *Options) *cobra.Command {
 			// If we have an email to use for slack, lets inform
 			if s.Application.AuthorEmail != "" {
 				// Setup Slack client
-				client, err := slack.NewClient(options.SlackToken, options.UserMappings)
+				client, err := slack.NewClient(options.SlackToken, options.UserMappings, options.EmailSuffix)
 				if err != nil {
 					fmt.Printf("Error creating Slack client")
 					return nil

cmd/artifact/command/push.go
@@ -31,7 +31,7 @@ func pushCommand(options *Options) *cobra.Command {
 					return err
 				}
 			}
-			client, err := slack.NewClient(options.SlackToken, options.UserMappings)
+			client, err := slack.NewClient(options.SlackToken, options.UserMappings, options.EmailSuffix)
 			if err != nil {
 				fmt.Printf("Error, not able to create Slack client in successful command: %v", err)
 				return nil

cmd/artifact/command/successful.go
@@ -14,7 +14,7 @@ func successfulCommand(options *Options) *cobra.Command {
 		Use:   "successful",
 		Short: "report successful in the pipeline",
 		RunE: func(c *cobra.Command, args []string) error {
-			client, err := slack.NewClient(options.SlackToken, options.UserMappings)
+			client, err := slack.NewClient(options.SlackToken, options.UserMappings, options.EmailSuffix)
 			if err != nil {
 				fmt.Printf("Error, not able to create Slack client in successful command: %v", err)
 				return nil

commit 4a5e90e5799a35cff0bfb24bab55d8a70ac98c81

Fix bug in server slack user lookup
Currently user-mappings on the server does not work. This is due to a bug
introduced in 73d80a895f35015e30975ef9b75fcaea14493926 where email suffix
validation was added to artifact.

The feature was not implemented correctly for the muteable slack client and thus
messages would be suppressed for non-slack workspace emails.

This change introduces the email-suffix flag to the server to let it know when
to lookup emails in the user mappings. To be able to test this the slack client
was changed to accept an interface of the upstream slack client instead of just
a token.

Makefile
@@ -48,6 +48,10 @@ test:
 generate_mock:
 	mockery --dir ./internal/policy --inpackage --name GitService
 	mockery --dir ./internal/flow --inpackage --name GitService
+	mockery --dir ./internal/slack --inpackage --name SlackClient
+	# update imports of the slack client as it is not correctly resolved by
+	# mockery on generation
+	goimports -w ./internal/slack/mock_SlackClient.go
 
 integration-test: rabbitmq-background
 	@echo "Running integration tests against RabbitMQ on localhost"

cmd/artifact/command/add.go
@@ -5,7 +5,8 @@ import (
 	"path"
 
 	"github.com/lunarway/release-manager/internal/artifact"
-	"github.com/lunarway/release-manager/internal/slack"
+	intslack "github.com/lunarway/release-manager/internal/slack"
+	"github.com/nlopes/slack"
 	"github.com/spf13/cobra"
 )
 
@@ -48,7 +49,7 @@ func appendTestSubCommand(options *Options) *cobra.Command {
 			if err != nil {
 				return err
 			}
-			err = notifySlack(options, fmt.Sprintf(":white_check_mark: *Test* (passed: %d, failed: %d, skipped: %d)", testData.Results.Passed, testData.Results.Failed, testData.Results.Skipped), slack.MsgColorYellow)
+			err = notifySlack(options, fmt.Sprintf(":white_check_mark: *Test* (passed: %d, failed: %d, skipped: %d)", testData.Results.Passed, testData.Results.Failed, testData.Results.Skipped), intslack.MsgColorYellow)
 			if err != nil {
 				fmt.Printf("Error notifying slack")
 			}
@@ -87,7 +88,7 @@ func appendBuildSubCommand(options *Options) *cobra.Command {
 			if err != nil {
 				return nil
 			}
-			err = notifySlack(options, fmt.Sprintf(":white_check_mark: *Build* (%s:%s)", buildData.Image, buildData.Tag), slack.MsgColorYellow)
+			err = notifySlack(options, fmt.Sprintf(":white_check_mark: *Build* (%s:%s)", buildData.Image, buildData.Tag), intslack.MsgColorYellow)
 			if err != nil {
 				fmt.Printf("Error notifying slack")
 			}
@@ -126,7 +127,7 @@ func appendSnykDockerSubCommand(options *Options) *cobra.Command {
 			if err != nil {
 				return err
 			}
-			err = notifySlack(options, fmt.Sprintf(":white_check_mark: <%s|*Snyk - Docker*> (high: %d, medium: %d, low: %d)", formatSnykURL(snykDockerData.URL), snykDockerData.Vulnerabilities.High, snykDockerData.Vulnerabilities.Medium, snykDockerData.Vulnerabilities.Low), slack.MsgColorYellow)
+			err = notifySlack(options, fmt.Sprintf(":white_check_mark: <%s|*Snyk - Docker*> (high: %d, medium: %d, low: %d)", formatSnykURL(snykDockerData.URL), snykDockerData.Vulnerabilities.High, snykDockerData.Vulnerabilities.Medium, snykDockerData.Vulnerabilities.Low), intslack.MsgColorYellow)
 			if err != nil {
 				fmt.Printf("Error notifying slack")
 			}
@@ -162,7 +163,7 @@ func appendSnykCodeSubCommand(options *Options) *cobra.Command {
 				return err
 			}
 
-			err = notifySlack(options, fmt.Sprintf(":white_check_mark: <%s|*Snyk - Code*> (high: %d, medium: %d, low: %d)", formatSnykURL(snykCodeData.URL), snykCodeData.Vulnerabilities.High, snykCodeData.Vulnerabilities.Medium, snykCodeData.Vulnerabilities.Low), slack.MsgColorYellow)
+			err = notifySlack(options, fmt.Sprintf(":white_check_mark: <%s|*Snyk - Code*> (high: %d, medium: %d, low: %d)", formatSnykURL(snykCodeData.URL), snykCodeData.Vulnerabilities.High, snykCodeData.Vulnerabilities.Medium, snykCodeData.Vulnerabilities.Low), intslack.MsgColorYellow)
 			if err != nil {
 				fmt.Printf("Error notifying slack")
 			}
@@ -196,7 +197,7 @@ func appendPushSubCommand(options *Options) *cobra.Command {
 			if err != nil {
 				return err
 			}
-			err = notifySlack(options, fmt.Sprintf(":white_check_mark: *Push* (%s:%s)", pushData.Image, pushData.Tag), slack.MsgColorYellow)
+			err = notifySlack(options, fmt.Sprintf(":white_check_mark: *Push* (%s:%s)", pushData.Image, pushData.Tag), intslack.MsgColorYellow)
 			if err != nil {
 				fmt.Printf("Error notifying slack")
 			}
@@ -240,12 +241,12 @@ func setStage(s artifact.Spec, stage artifact.Stage) artifact.Spec {
 }
 
 func notifySlack(options *Options, text, color string) error {
-	client, err := slack.NewClient(options.SlackToken, options.UserMappings, options.EmailSuffix)
+	client, err := intslack.NewClient(slack.New(options.SlackToken), options.UserMappings, options.EmailSuffix)
 	if err != nil {
 		return err
 	}
 	messageFilePath := path.Join(options.RootPath, options.MessageFileName)
-	err = client.UpdateMessage(messageFilePath, func(m slack.Message) slack.Message {
+	err = client.UpdateMessage(messageFilePath, func(m intslack.Message) intslack.Message {
 		m.Text += text + "\n"
 		m.Color = color
 		return m

cmd/artifact/command/failure.go
@@ -5,7 +5,8 @@ import (
 	"path"
 
 	"github.com/lunarway/release-manager/internal/artifact"
-	"github.com/lunarway/release-manager/internal/slack"
+	intslack "github.com/lunarway/release-manager/internal/slack"
+	"github.com/nlopes/slack"
 	"github.com/spf13/cobra"
 )
 
@@ -15,13 +16,13 @@ func failureCommand(options *Options) *cobra.Command {
 		Use:   "failure",
 		Short: "report failure in the pipeline",
 		RunE: func(c *cobra.Command, args []string) error {
-			client, err := slack.NewClient(options.SlackToken, options.UserMappings, options.EmailSuffix)
+			client, err := intslack.NewClient(slack.New(options.SlackToken), options.UserMappings, options.EmailSuffix)
 			if err != nil {
 				fmt.Printf("Error, not able to create Slack client in failure command: %v", err)
 				return nil
 			}
-			err = client.UpdateMessage(path.Join(options.RootPath, options.MessageFileName), func(m slack.Message) slack.Message {
-				m.Color = slack.MsgColorRed
+			err = client.UpdateMessage(path.Join(options.RootPath, options.MessageFileName), func(m intslack.Message) intslack.Message {
+				m.Color = intslack.MsgColorRed
 				m.Text += fmt.Sprintf(":no_entry: *%s*", errorMessage)
 				m.Title = ":jenkins: Jenkins :no_entry:"
 				return m
@@ -37,7 +38,7 @@ func failureCommand(options *Options) *cobra.Command {
 				return nil
 			}
 
-			err = client.NotifySlackBuildsChannel(slack.BuildsOptions{
+			err = client.NotifySlackBuildsChannel(intslack.BuildsOptions{
 				Service:       a.Service,
 				ArtifactID:    a.ID,
 				Branch:        a.Application.Branch,
@@ -46,7 +47,7 @@ func failureCommand(options *Options) *cobra.Command {
 				CommitAuthor:  a.Application.AuthorName,
 				CommitMessage: a.Application.Message,
 				CIJobURL:      a.CI.JobURL,
-				Color:         slack.MsgColorRed,
+				Color:         intslack.MsgColorRed,
 			})
 			if err != nil {
 				fmt.Printf("Error, not able to notify #builds in failure command: %v", err)

cmd/artifact/command/init.go
@@ -8,7 +8,8 @@ import (
 	"time"
 
 	"github.com/lunarway/release-manager/internal/artifact"
-	"github.com/lunarway/release-manager/internal/slack"
+	intslack "github.com/lunarway/release-manager/internal/slack"
+	"github.com/nlopes/slack"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -28,7 +29,7 @@ func initCommand(options *Options) *cobra.Command {
 				users = strings.Split(userMappingString, ",")
 			}
 			var err error
-			options.UserMappings, err = slack.ParseUserMappings(users)
+			options.UserMappings, err = intslack.ParseUserMappings(users)
 			if err != nil {
 				return err
 			}
@@ -71,7 +72,7 @@ func initCommand(options *Options) *cobra.Command {
 			// If we have an email to use for slack, lets inform
 			if s.Application.AuthorEmail != "" {
 				// Setup Slack client
-				client, err := slack.NewClient(options.SlackToken, options.UserMappings, options.EmailSuffix)
+				client, err := intslack.NewClient(slack.New(options.SlackToken), options.UserMappings, options.EmailSuffix)
 				if err != nil {
 					fmt.Printf("Error creating Slack client")
 					return nil
@@ -81,7 +82,7 @@ func initCommand(options *Options) *cobra.Command {
 				title := ":jenkins: Jenkins :sonic-running:"
 				titleLink := s.CI.JobURL
 				text := fmt.Sprintf("Build for: %s\nBranch: <%s|*%s*>\n", s.Application.Name, s.Application.URL, s.Application.Branch)
-				color := slack.MsgColorYellow
+				color := intslack.MsgColorYellow
 				respChan, timestamp, err := client.PostSlackBuildStarted(s.Application.AuthorEmail, title, titleLink, text, color)
 				if err != nil {
 					return nil
@@ -89,7 +90,7 @@ func initCommand(options *Options) *cobra.Command {
 
 				// Persist the Slack message to disk for later retrieval and updates
 				messageFilePath := path.Join(options.RootPath, options.MessageFileName)
-				err = slack.Persist(messageFilePath, slack.Message{
+				err = intslack.Persist(messageFilePath, intslack.Message{
 					Title:     title,
 					TitleLink: titleLink,
 					Text:      text,

cmd/artifact/command/push.go
@@ -8,7 +8,8 @@ import (
 
 	"github.com/lunarway/release-manager/internal/flow"
 	httpinternal "github.com/lunarway/release-manager/internal/http"
-	"github.com/lunarway/release-manager/internal/slack"
+	intslack "github.com/lunarway/release-manager/internal/slack"
+	"github.com/nlopes/slack"
 	"github.com/spf13/cobra"
 )
 
@@ -32,12 +33,12 @@ func pushCommand(options *Options) *cobra.Command {
 					return err
 				}
 			}
-			client, err := slack.NewClient(options.SlackToken, options.UserMappings, options.EmailSuffix)
+			client, err := intslack.NewClient(slack.New(options.SlackToken), options.UserMappings, options.EmailSuffix)
 			if err != nil {
 				fmt.Printf("Error, not able to create Slack client in successful command: %v", err)
 				return nil
 			}
-			err = client.UpdateMessage(path.Join(options.RootPath, options.MessageFileName), func(m slack.Message) slack.Message {
+			err = client.UpdateMessage(path.Join(options.RootPath, options.MessageFileName), func(m intslack.Message) intslack.Message {
 				m.Text += fmt.Sprintf(":white_check_mark: *Artifact pushed:* %s", artifactID)
 				return m
 			})

cmd/artifact/command/successful.go
@@ -5,7 +5,8 @@ import (
 	"path"
 
 	"github.com/lunarway/release-manager/internal/artifact"
-	"github.com/lunarway/release-manager/internal/slack"
+	intslack "github.com/lunarway/release-manager/internal/slack"
+	"github.com/nlopes/slack"
 	"github.com/spf13/cobra"
 )
 
@@ -14,13 +15,13 @@ func successfulCommand(options *Options) *cobra.Command {
 		Use:   "successful",
 		Short: "report successful in the pipeline",
 		RunE: func(c *cobra.Command, args []string) error {
-			client, err := slack.NewClient(options.SlackToken, options.UserMappings, options.EmailSuffix)
+			client, err := intslack.NewClient(slack.New(options.SlackToken), options.UserMappings, options.EmailSuffix)
 			if err != nil {
 				fmt.Printf("Error, not able to create Slack client in successful command: %v", err)
 				return nil
 			}
-			err = client.UpdateMessage(path.Join(options.RootPath, options.MessageFileName), func(m slack.Message) slack.Message {
-				m.Color = slack.MsgColorGreen
+			err = client.UpdateMessage(path.Join(options.RootPath, options.MessageFileName), func(m intslack.Message) intslack.Message {
+				m.Color = intslack.MsgColorGreen
 				m.Title = ":jenkins: Jenkins :white_check_mark:"
 				return m
 			})
@@ -35,7 +36,7 @@ func successfulCommand(options *Options) *cobra.Command {
 				return nil
 			}
 
-			err = client.NotifySlackBuildsChannel(slack.BuildsOptions{
+			err = client.NotifySlackBuildsChannel(intslack.BuildsOptions{
 				Service:       a.Service,
 				ArtifactID:    a.ID,
 				Branch:        a.Application.Branch,
@@ -44,7 +45,7 @@ func successfulCommand(options *Options) *cobra.Command {
 				CommitAuthor:  a.Application.AuthorName,
 				CommitMessage: a.Application.Message,
 				CIJobURL:      a.CI.JobURL,
-				Color:         slack.MsgColorGreen,
+				Color:         intslack.MsgColorGreen,
 			})
 			if err != nil {
 				fmt.Printf("Error, not able to notify #builds in successful command: %v", err)

cmd/server/command/root.go
@@ -31,6 +31,7 @@ func NewCommand() (*cobra.Command, error) {
 	var logConfiguration *log.Configuration
 	var slackMuteOpts slack.MuteOptions
 	var s3storageOpts s3storageOptions
+	var emailSuffix string
 
 	var command = &cobra.Command{
 		Use:   "server",
@@ -71,6 +72,7 @@ func NewCommand() (*cobra.Command, error) {
 		slackMutes:                &slackMuteOpts,
 		userMappings:              &userMappings,
 		branchRestrictionPolicies: &branchRestrictions,
+		emailSuffix:               &emailSuffix,
 	}))
 	command.PersistentFlags().IntVar(&httpOpts.Port, "http-port", 8080, "port of the http server")
 	command.PersistentFlags().DurationVar(&httpOpts.Timeout, "timeout", 20*time.Second, "HTTP server timeout for incomming requests")
@@ -89,6 +91,7 @@ func NewCommand() (*cobra.Command, error) {
 	command.PersistentFlags().StringVar(&grafanaOpts.DevURL, "grafana-dev-url", os.Getenv("GRAFANA_DEV_URL"), "grafana dev url")
 	command.PersistentFlags().StringVar(&grafanaOpts.StagingURL, "grafana-staging-url", os.Getenv("GRAFANA_STAGING_URL"), "grafana staging url")
 	command.PersistentFlags().StringVar(&grafanaOpts.ProdURL, "grafana-prod-url", os.Getenv("GRAFANA_PROD_URL"), "grafana prod url")
+	command.PersistentFlags().StringVar(&emailSuffix, "email-suffix", "", "company email suffix to expect. E.g.: '@example.com'")
 	command.PersistentFlags().StringSliceVar(&users, "user-mappings", []string{}, "user mappings between emails used by Git and Slack, key-value pair: <email>=<slack-email>")
 	command.PersistentFlags().StringSliceVar(&branchRestrictionsList, "policy-branch-restrictions", []string{}, "branch restriction policies applied to all releases, key-value pair: <environment>=<branch-regex>")
 	command.PersistentFlags().StringSliceVar(&gpgKeyPaths, "git-gpg-key-import-paths", []string{}, "a list of paths for signing keys to import to gpg")

cmd/server/command/start.go
@@ -21,8 +21,9 @@ import (
 	"github.com/lunarway/release-manager/internal/log"
 	"github.com/lunarway/release-manager/internal/policy"
 	"github.com/lunarway/release-manager/internal/s3storage"
-	"github.com/lunarway/release-manager/internal/slack"
+	intslack "github.com/lunarway/release-manager/internal/slack"
 	"github.com/lunarway/release-manager/internal/tracing"
+	"github.com/nlopes/slack"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -102,10 +103,11 @@ type startOptions struct {
 	http                      *http.Options
 	broker                    *brokerOptions
 	s3storage                 *s3storageOptions
-	slackMutes                *slack.MuteOptions
+	slackMutes                *intslack.MuteOptions
 	gpgKeyPaths               *[]string
 	userMappings              *map[string]string
 	branchRestrictionPolicies *[]policy.BranchRestriction
+	emailSuffix               *string
 }
 
 func NewStart(startOptions *startOptions) *cobra.Command {
@@ -114,7 +116,7 @@ func NewStart(startOptions *startOptions) *cobra.Command {
 		Short: "start the release-manager",
 		RunE: func(c *cobra.Command, args []string) error {
 			done := make(chan error, 1)
-			slackClient, err := slack.NewMuteableClient(*startOptions.slackAuthToken, *startOptions.userMappings, *startOptions.slackMutes)
+			slackClient, err := intslack.NewMuteableClient(slack.New(*startOptions.slackAuthToken), *startOptions.userMappings, *startOptions.emailSuffix, *startOptions.slackMutes)
 			if err != nil {
 				return err
 			}
@@ -224,7 +226,7 @@ func NewStart(startOptions *startOptions) *cobra.Command {
 						"releaser", opts.Releaser,
 						"type", "release")
 
-					releaseOptions := slack.ReleaseOptions{
+					releaseOptions := intslack.ReleaseOptions{
 						Service:           opts.Service,
 						Environment:       opts.Environment,
 						ArtifactID:        opts.Spec.ID,

internal/flow/mock_GitService.go
@@ -1,4 +1,4 @@
-// Code generated by mockery v2.1.0. DO NOT EDIT.
+// Code generated by mockery v2.5.1. DO NOT EDIT.
 
 package flow
 

internal/policy/mock_GitService.go
@@ -1,4 +1,4 @@
-// Code generated by mockery v2.1.0. DO NOT EDIT.
+// Code generated by mockery v2.5.1. DO NOT EDIT.
 
 package policy
 

internal/slack/mock_SlackClient.go
@@ -1,0 +1,150 @@
+// Code generated by mockery v2.5.1. DO NOT EDIT.
+
+package slack
+
+import (
+	context "context"
+
+	"github.com/nlopes/slack"
+	mock "github.com/stretchr/testify/mock"
+)
+
+// MockSlackClient is an autogenerated mock type for the SlackClient type
+type MockSlackClient struct {
+	mock.Mock
+}
+
+// GetUserByEmailContext provides a mock function with given fields: ctx, email
+func (_m *MockSlackClient) GetUserByEmailContext(ctx context.Context, email string) (*slack.User, error) {
+	ret := _m.Called(ctx, email)
+
+	var r0 *slack.User
+	if rf, ok := ret.Get(0).(func(context.Context, string) *slack.User); ok {
+		r0 = rf(ctx, email)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*slack.User)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, email)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// PostMessage provides a mock function with given fields: channelID, options
+func (_m *MockSlackClient) PostMessage(channelID string, options ...slack.MsgOption) (string, string, error) {
+	_va := make([]interface{}, len(options))
+	for _i := range options {
+		_va[_i] = options[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, channelID)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func(string, ...slack.MsgOption) string); ok {
+		r0 = rf(channelID, options...)
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	var r1 string
+	if rf, ok := ret.Get(1).(func(string, ...slack.MsgOption) string); ok {
+		r1 = rf(channelID, options...)
+	} else {
+		r1 = ret.Get(1).(string)
+	}
+
+	var r2 error
+	if rf, ok := ret.Get(2).(func(string, ...slack.MsgOption) error); ok {
+		r2 = rf(channelID, options...)
+	} else {
+		r2 = ret.Error(2)
+	}
+
+	return r0, r1, r2
+}
+
+// PostMessageContext provides a mock function with given fields: ctx, channelID, options
+func (_m *MockSlackClient) PostMessageContext(ctx context.Context, channelID string, options ...slack.MsgOption) (string, string, error) {
+	_va := make([]interface{}, len(options))
+	for _i := range options {
+		_va[_i] = options[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, channelID)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func(context.Context, string, ...slack.MsgOption) string); ok {
+		r0 = rf(ctx, channelID, options...)
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	var r1 string
+	if rf, ok := ret.Get(1).(func(context.Context, string, ...slack.MsgOption) string); ok {
+		r1 = rf(ctx, channelID, options...)
+	} else {
+		r1 = ret.Get(1).(string)
+	}
+
+	var r2 error
+	if rf, ok := ret.Get(2).(func(context.Context, string, ...slack.MsgOption) error); ok {
+		r2 = rf(ctx, channelID, options...)
+	} else {
+		r2 = ret.Error(2)
+	}
+
+	return r0, r1, r2
+}
+
+// UpdateMessage provides a mock function with given fields: channelID, timestamp, options
+func (_m *MockSlackClient) UpdateMessage(channelID string, timestamp string, options ...slack.MsgOption) (string, string, string, error) {
+	_va := make([]interface{}, len(options))
+	for _i := range options {
+		_va[_i] = options[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, channelID, timestamp)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func(string, string, ...slack.MsgOption) string); ok {
+		r0 = rf(channelID, timestamp, options...)
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	var r1 string
+	if rf, ok := ret.Get(1).(func(string, string, ...slack.MsgOption) string); ok {
+		r1 = rf(channelID, timestamp, options...)
+	} else {
+		r1 = ret.Get(1).(string)
+	}
+
+	var r2 string
+	if rf, ok := ret.Get(2).(func(string, string, ...slack.MsgOption) string); ok {
+		r2 = rf(channelID, timestamp, options...)
+	} else {
+		r2 = ret.Get(2).(string)
+	}
+
+	var r3 error
+	if rf, ok := ret.Get(3).(func(string, string, ...slack.MsgOption) error); ok {
+		r3 = rf(channelID, timestamp, options...)
+	} else {
+		r3 = ret.Error(3)
+	}
+
+	return r0, r1, r2, r3
+}

internal/slack/slack.go
@@ -11,8 +11,15 @@ import (
 	"github.com/pkg/errors"
 )
 
+type SlackClient interface {
+	GetUserByEmailContext(ctx context.Context, email string) (*slack.User, error)
+	UpdateMessage(channelID, timestamp string, options ...slack.MsgOption) (string, string, string, error)
+	PostMessage(channelID string, options ...slack.MsgOption) (string, string, error)
+	PostMessageContext(ctx context.Context, channelID string, options ...slack.MsgOption) (string, string, error)
+}
+
 type Client struct {
-	client        *slack.Client
+	client        SlackClient
 	emailMappings map[string]string
 	muteOptions   MuteOptions
 	emailSuffix   string
@@ -34,8 +41,8 @@ var (
 	ErrUnknownEmail = errors.New("not an accepted email domain")
 )
 
-func NewClient(token string, emailMappings map[string]string, emailSuffix string) (*Client, error) {
-	if token == "" {
+func NewClient(slackClient SlackClient, emailMappings map[string]string, emailSuffix string) (*Client, error) {
+	if slackClient == nil {
 		log.Infof("slack: skipping: no token, so no slack notification")
 		return &Client{
 			muteOptions: MuteOptions{
@@ -51,40 +58,22 @@ func NewClient(token string, emailMappings map[string]string, emailSuffix string
 	}
 
 	log.Infof("slack: new client: initialized with emailMappings: %+v", emailMappings)
-	slackClient := slack.New(token)
 	client := Client{
 		client:        slackClient,
 		emailMappings: emailMappings,
 		muteOptions:   MuteOptions{},
+		emailSuffix:   emailSuffix,
 	}
-	client.emailSuffix = emailSuffix
 	return &client, nil
 }
 
-func NewMuteableClient(token string, emailMappings map[string]string, muteOptions MuteOptions) (*Client, error) {
-	if token == "" {
-		log.Infof("slack: skipping: no token, so no slack notification")
-		return &Client{
-			muteOptions: MuteOptions{
-				Flux:                true,
-				Kubernetes:          true,
-				Policy:              true,
-				ReleaseProcessed:    true,
-				Releases:            true,
-				BuildStatus:         true,
-				ReleaseManagerError: true,
-			},
-		}, nil
+func NewMuteableClient(slackClient SlackClient, emailMappings map[string]string, emailSuffix string, muteOptions MuteOptions) (*Client, error) {
+	client, err := NewClient(slackClient, emailMappings, emailSuffix)
+	if err != nil {
+		return nil, err
 	}
-
-	log.Infof("slack: new client: initialized with emailMappings: %+v", emailMappings)
-	slackClient := slack.New(token)
-	client := Client{
-		client:        slackClient,
-		emailMappings: emailMappings,
-		muteOptions:   muteOptions,
-	}
-	return &client, nil
+	client.muteOptions = muteOptions
+	return client, nil
 }
 
 func (c *Client) getIdByEmail(ctx context.Context, email string) (string, error) {

internal/slack/slack_test.go
@@ -1,0 +1,56 @@
+package slack_test
+
+import (
+	"testing"
+
+	"github.com/lunarway/release-manager/internal/log"
+	intslack "github.com/lunarway/release-manager/internal/slack"
+	"github.com/nlopes/slack"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap/zapcore"
+)
+
+// TestNewMutableClient_mapping tests that a mutable client handles email
+// mappings correctly. This test is added to fix a bug introduced in
+// 73d80a895f35015e30975ef9b75fcaea14493926 where domain verification was added
+// to slack messages. The feature was only implemented in the default Client
+// (non mutable) which made the server unable to send messages to emails to
+// non-slack emails.
+func TestNewMutableClient_mapping(t *testing.T) {
+	log.Init(&log.Configuration{
+		Level: log.Level{
+			Level: zapcore.DebugLevel,
+		},
+		Development: true,
+	})
+	var (
+		email       = "outside@mail.com"
+		emailDomain = "corp.com"
+		mapping     = map[string]string{
+			"outside@mail.com": "me@corp.com",
+		}
+		slackUser = &slack.User{
+			ID: "user-id",
+		}
+	)
+
+	slackClient := intslack.MockSlackClient{}
+	slackClient.Test(t)
+	slackClient.On("PostMessage", slackUser.ID, mock.Anything, mock.Anything).Return("", "", nil)
+	// return error when looking up non-corp email
+	slackClient.On("GetUserByEmailContext", mock.Anything, email).Return(&slack.User{}, errors.New("users_not_found"))
+	// return slack user on anything else
+	slackClient.On("GetUserByEmailContext", mock.Anything, mock.Anything).Return(slackUser, nil)
+
+	client, err := intslack.NewMuteableClient(&slackClient, mapping, emailDomain, intslack.MuteOptions{})
+	if !assert.NoError(t, err, "unexpected instantiation error") {
+		return
+	}
+
+	_, _, err = client.PostSlackBuildStarted(email, "title", "titleLink", "text", "color")
+	if !assert.NoError(t, err, "unexpected instantiation error") {
+		return
+	}
+}