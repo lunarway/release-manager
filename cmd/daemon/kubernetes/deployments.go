a7d75cc4563e0a9bba9439090a3229be3dc3548c
Bjørn Sørensen <bso@lunar.app>
2021-11-03 21:37:27 +0100

    No startup sync with an informer

--- cmd/daemon/kubernetes/deployments.go
+++ cmd/daemon/kubernetes/deployments.go
@@ -2,89 +2,126 @@ package kubernetes
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/lunarway/release-manager/internal/http"
 	"github.com/lunarway/release-manager/internal/log"
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 	deploymentutil "k8s.io/kubernetes/pkg/controller/deployment/util"
 )
 
 func (c *Client) HandleNewDeployments(ctx context.Context) error {
-	watcher, err := c.clientset.AppsV1().Deployments("").Watch(ctx, metav1.ListOptions{})
-	if err != nil {
-		return err
+	log.Info("Watching deployments")
+	factory := informers.NewSharedInformerFactory(c.clientset, 0)
+	deploymentInformer := factory.Apps().V1().Deployments()
+
+	readyToProcess := make(chan struct{})
+
+	deploymentInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			if !hasSynced(readyToProcess) {
+				return
+			}
+			log.Infof("Got Add: %+v", obj)
+			c.handleDeployment(obj)
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			if !hasSynced(readyToProcess) {
+				return
+			}
+			log.Infof("Got Update: %+v", newObj)
+			c.handleDeployment(newObj)
+		},
+		DeleteFunc: func(obj interface{}) {
+			if !hasSynced(readyToProcess) {
+				return
+			}
+			log.Infof("Got Delete: doing nothing: %+v", obj)
+		},
+	})
+
+	stopCh := make(chan struct{})
+	factory.Start(stopCh)
+	if !cache.WaitForCacheSync(stopCh, deploymentInformer.Informer().HasSynced) {
+		return fmt.Errorf("failed to sync deployment informer")
 	}
-	for {
-		select {
-		case <-ctx.Done():
-			watcher.Stop()
-		case e, ok := <-watcher.ResultChan():
-			if !ok {
-				return ErrWatcherClosed
-			}
-			if e.Object == nil {
-				continue
-			}
-			if e.Type == watch.Deleted {
-				continue
-			}
-			deploy, ok := e.Object.(*appsv1.Deployment)
-			if !ok {
-				continue
-			}
 
-			// Check if we have all the annotations we need for the release-daemon
-			if !isCorrectlyAnnotated(deploy.Annotations) {
-				continue
-			}
+	log.Infof("Last synced: %s", deploymentInformer.Informer().LastSyncResourceVersion())
+	close(readyToProcess)
+	<-ctx.Done()
+	close(stopCh)
 
-			// Avoid reporting on pods that has been marked for termination
-			if isDeploymentMarkedForTermination(deploy) {
-				continue
-			}
+	return nil
+}
 
-			// Check the received event, and determine whether or not this was a successful deployment.
-			if !isDeploymentSuccessful(deploy) {
-				continue
-			}
+func hasSynced(readyToProcess chan struct{}) bool {
+	select {
+	case <-readyToProcess:
+		return true
+	default:
+		return false
+	}
+}
 
-			// In-order to minimize messages and only return events when new releases is detected, we add
-			// a new annotation to the Deployment.
-			// When we initially apply a Deployment the lunarway.com/artifact-id annotations SHOULD be set.
-			// Further the observed-artifact-id is an annotation managed by the daemon and will initially be "".
-			// In this state we annotate the Deployment with the current artifact-id as the observed.
-			// When we update a Deployment we also update the artifact-id, e.g. now observed and actual artifact id
-			// is different. In this case we want to notify, and update the observed with the current artifact id.
-			// This also eliminates messages when a pod is deleted. As the two annotations will be equal.
-			if deploy.Annotations["lunarway.com/observed-artifact-id"] == deploy.Annotations["lunarway.com/artifact-id"] {
-				continue
-			}
+func (c *Client) handleDeployment(e interface{}) {
+	deploy, ok := e.(*appsv1.Deployment)
+	if !ok {
+		return
+	}
 
-			// Notify the release-manager with the successful deployment event.
-			err = c.exporter.SendSuccessfulReleaseEvent(ctx, http.ReleaseEvent{
-				Name:          deploy.Name,
-				Namespace:     deploy.Namespace,
-				ResourceType:  "Deployment",
-				ArtifactID:    deploy.Annotations["lunarway.com/artifact-id"],
-				AuthorEmail:   deploy.Annotations["lunarway.com/author"],
-				AvailablePods: deploy.Status.AvailableReplicas,
-				DesiredPods:   *deploy.Spec.Replicas,
-			})
-			if err != nil {
-				log.Errorf("Failed to send successful deployment event: %v", err)
-				continue
-			}
+	ctx := context.Background()
 
-			// Annotate the Deployment to be able to skip it next time
-			err = annotateDeployment(ctx, c.clientset, deploy)
-			if err != nil {
-				log.Errorf("Unable to annotate Deployment: %v", err)
-				continue
-			}
-		}
+	// Check if we have all the annotations we need for the release-daemon
+	if !isCorrectlyAnnotated(deploy.Annotations) {
+		return
+	}
+
+	// Avoid reporting on pods that has been marked for termination
+	if isDeploymentMarkedForTermination(deploy) {
+		return
+	}
+
+	// Check the received event, and determine whether or not this was a successful deployment.
+	if !isDeploymentSuccessful(deploy) {
+		return
+	}
+
+	// In-order to minimize messages and only return events when new releases is detected, we add
+	// a new annotation to the Deployment.
+	// When we initially apply a Deployment the lunarway.com/artifact-id annotations SHOULD be set.
+	// Further the observed-artifact-id is an annotation managed by the daemon and will initially be "".
+	// In this state we annotate the Deployment with the current artifact-id as the observed.
+	// When we update a Deployment we also update the artifact-id, e.g. now observed and actual artifact id
+	// is different. In this case we want to notify, and update the observed with the current artifact id.
+	// This also eliminates messages when a pod is deleted. As the two annotations will be equal.
+	if deploy.Annotations["lunarway.com/observed-artifact-id"] == deploy.Annotations["lunarway.com/artifact-id"] {
+		return
+	}
+
+	// Notify the release-manager with the successful deployment event.
+	err := c.exporter.SendSuccessfulReleaseEvent(ctx, http.ReleaseEvent{
+		Name:          deploy.Name,
+		Namespace:     deploy.Namespace,
+		ResourceType:  "Deployment",
+		ArtifactID:    deploy.Annotations["lunarway.com/artifact-id"],
+		AuthorEmail:   deploy.Annotations["lunarway.com/author"],
+		AvailablePods: deploy.Status.AvailableReplicas,
+		DesiredPods:   *deploy.Spec.Replicas,
+	})
+	if err != nil {
+		log.Errorf("Failed to send successful deployment event: %v", err)
+		return
+	}
+
+	// Annotate the Deployment to be able to skip it next time
+	err = annotateDeployment(ctx, c.clientset, deploy)
+	if err != nil {
+		log.Errorf("Unable to annotate Deployment: %v", err)
+		return
 	}
 }
 
