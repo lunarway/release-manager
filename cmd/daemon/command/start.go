5dacb07df166da108a625dca716e7a3c304fe28c
Martin Anker Have <mah@lunar.app>
2022-05-30 14:31:41 +0200

    Fix issue where the k8s observers where never started due to a block http server call

--- cmd/daemon/command/start.go
+++ cmd/daemon/command/start.go
@@ -57,7 +57,14 @@ func StartDaemon() *cobra.Command {
 			kubernetes.RegisterJobInformer(kubectl.InformerFactory, exporter, handlerFactory, kubectl.Clientset)
 			kubernetes.RegisterPodInformer(kubectl.InformerFactory, exporter, handlerFactory, kubectl.Clientset, moduloCrashReportNotif)
 			kubernetes.RegisterStatefulSetInformer(kubectl.InformerFactory, exporter, handlerFactory, kubectl.Clientset)
-			flux_notification_controller.StartHttpServer()
+			server := flux_notification_controller.NewHttpServer()
+			go func() {
+				err := server.ListenAndServe()
+				if err != nil {
+					log.Errorf("Failed to start the notification server: %s", err)
+					os.Exit(1)
+				}
+			}()
 
 			log.Info("Deamon started")
 
@@ -80,6 +87,13 @@ func StartDaemon() *cobra.Command {
 				log.Errorf("Exited unknown error: %v", err)
 				os.Exit(1)
 			}
+
+			err = server.Close()
+			if err != nil {
+				log.Errorf("Failed to close the notification server: %s", err)
+				os.Exit(1)
+			}
+
 			log.Infof("Program ended")
 			return nil
 		},
