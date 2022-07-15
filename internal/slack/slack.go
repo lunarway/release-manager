4d9b70cb15921def7cb79a31cc1203dd1748dc83
Maria Frandsen <mcf@lunar.app>
2022-07-15 10:12:23 +0200

    get crashloopbackoff events into platform squads alert channel

--- internal/slack/slack.go
+++ internal/slack/slack.go
@@ -313,6 +313,9 @@ func (c *Client) NotifyK8SPodErrorEvent(ctx context.Context, event *http.PodErro
 	if err != nil {
 		return err
 	}
+	if event.Squad == "aura" || event.Squad == "odyssey" {
+		_, _, err = c.client.PostMessageContext(ctx, fmt.Sprintf("#squad-%s-alerts", event.Squad), asUser, attachments)
+	}
 	return err
 }
 
