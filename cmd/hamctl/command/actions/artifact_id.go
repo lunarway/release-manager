lunarway/release-manager

commit 1482e499a5f3fada4434e183fc0743145bb62019

Promote using release (#195)
* Refactor hamctl promote to use describe and release endpoints
* Add release intent
* Remove promote from server
* Improve release endpoints logs and http feedback with intent

cmd/hamctl/command/actions/artifact_id.go
@@ -1,0 +1,57 @@
+package actions
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	httpinternal "github.com/lunarway/release-manager/internal/http"
+)
+
+func ArtifactIDFromEnvironment(client *httpinternal.Client, service, namespace, environment string) (string, error) {
+	var statusResp httpinternal.StatusResponse
+	params := url.Values{}
+	params.Add("service", service)
+	if namespace != "" {
+		params.Add("namespace", namespace)
+	}
+	path, err := client.URLWithQuery("status", params)
+	if err != nil {
+		return "", err
+	}
+	err = client.Do(http.MethodGet, path, nil, &statusResp)
+	if err != nil {
+		return "", err
+	}
+
+	switch environment {
+	case "dev":
+		return statusResp.Dev.Tag, nil
+	case "staging":
+		return statusResp.Staging.Tag, nil
+	case "prod":
+		return statusResp.Prod.Tag, nil
+	}
+
+	return "", fmt.Errorf("unknown environment %s", environment)
+}
+
+func ArtifactIDFromBranch(client *httpinternal.Client, service string, branch string) (string, error) {
+	var describeResp httpinternal.DescribeArtifactResponse
+	params := url.Values{}
+	params.Add("branch", branch)
+	path, err := client.URLWithQuery(fmt.Sprintf("describe/latest-artifact/%s/%s", service, branch), params)
+	if err != nil {
+		return "", err
+	}
+	err = client.Do(http.MethodGet, path, nil, &describeResp)
+	if err != nil {
+		return "", err
+	}
+
+	if len(describeResp.Artifacts) == 0 {
+		return "", fmt.Errorf("no artifacts found on from branch '%s'", branch)
+	}
+
+	return describeResp.Artifacts[0].ID, nil
+}

cmd/server/http/http.go
@@ -35,7 +35,6 @@ func NewServer(opts *Options, slackClient *slack.Client, flowSvc *flow.Service, 
 	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", ping)
-	mux.HandleFunc("/promote", trace(tracer, authenticate(opts.HamCtlAuthToken, promote(&payloader, flowSvc))))
 	mux.HandleFunc("/release", trace(tracer, authenticate(opts.HamCtlAuthToken, release(&payloader, flowSvc))))
 	mux.HandleFunc("/status", trace(tracer, authenticate(opts.HamCtlAuthToken, status(&payloader, flowSvc))))
 	mux.HandleFunc("/rollback", trace(tracer, authenticate(opts.HamCtlAuthToken, rollback(&payloader, flowSvc))))

cmd/hamctl/command/actions/release.go
@@ -1,0 +1,40 @@
+package actions
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/lunarway/release-manager/internal/git"
+	httpinternal "github.com/lunarway/release-manager/internal/http"
+	"github.com/lunarway/release-manager/internal/intent"
+)
+
+func ReleaseArtifactID(client *httpinternal.Client, service, environment, artifactID string, intent intent.Intent) error {
+	committerName, committerEmail, err := git.CommitterDetails()
+	if err != nil {
+		return err
+	}
+	var resp httpinternal.ReleaseResponse
+	path, err := client.URL("release")
+	if err != nil {
+		return err
+	}
+	err = client.Do(http.MethodPost, path, httpinternal.ReleaseRequest{
+		Service:        service,
+		Environment:    environment,
+		ArtifactID:     artifactID,
+		CommitterName:  committerName,
+		CommitterEmail: committerEmail,
+		Intent:         intent,
+	}, &resp)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Release of service: %s\n", service)
+	if resp.Status != "" {
+		fmt.Printf("%s\n", resp.Status)
+	} else {
+		fmt.Printf("[✓] Release of %s to %s initialized\n", resp.Tag, resp.ToEnvironment)
+	}
+	return nil
+}

cmd/hamctl/command/promote.go
@@ -1,17 +1,15 @@
 package command
 
 import (
-	"fmt"
-	"net/http"
-
+	"github.com/lunarway/release-manager/cmd/hamctl/command/actions"
 	"github.com/lunarway/release-manager/cmd/hamctl/command/completion"
-	"github.com/lunarway/release-manager/internal/git"
 	httpinternal "github.com/lunarway/release-manager/internal/http"
+	"github.com/lunarway/release-manager/internal/intent"
 	"github.com/spf13/cobra"
 )
 
 func NewPromote(client *httpinternal.Client, service *string) *cobra.Command {
-	var environment, namespace string
+	var toEnvironment, fromEnvironment, namespace string
 	var command = &cobra.Command{
 		Use:   "promote",
 		Short: "Promote a service to a specific environment following promoting conventions.",
@@ -21,43 +19,39 @@ func NewPromote(client *httpinternal.Client, service *string) *cobra.Command {
 			})
 		},
 		RunE: func(c *cobra.Command, args []string) error {
-			committerName, committerEmail, err := git.CommitterDetails()
-			if err != nil {
-				return err
+			if fromEnvironment == "" {
+				switch toEnvironment {
+				case "dev":
+					fromEnvironment = "master"
+				case "staging":
+					fromEnvironment = "dev"
+				case "prod":
+					fromEnvironment = "staging"
+				}
 			}
-
-			var resp httpinternal.PromoteResponse
-			url, err := client.URL("promote")
-			if err != nil {
-				return err
-			}
-			err = client.Do(http.MethodPost, url, httpinternal.PromoteRequest{
-				Service:        *service,
-				Environment:    environment,
-				Namespace:      namespace,
-				CommitterName:  committerName,
-				CommitterEmail: committerEmail,
-			}, &resp)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("Promote service: %s\n", *service)
-			if resp.Status != "" {
-				fmt.Printf("%s\n", resp.Status)
+			var artifactID string
+			var err error
+			if fromEnvironment == "master" {
+				artifactID, err = actions.ArtifactIDFromBranch(client, *service, "master")
 			} else {
-				fmt.Printf("[✓] Promotion of %s from %s to %s initialized\n", resp.Tag, resp.FromEnvironment, resp.ToEnvironment)
+				artifactID, err = actions.ArtifactIDFromEnvironment(client, *service, namespace, fromEnvironment)
+			}
+			if err != nil {
+				return err
 			}
 
-			return nil
+			return actions.ReleaseArtifactID(client, *service, toEnvironment, artifactID, intent.NewPromoteEnvironment(fromEnvironment))
 		},
 	}
-	command.Flags().StringVarP(&environment, "env", "e", "", "Environment to promote to (required)")
+	command.Flags().StringVarP(&toEnvironment, "env", "e", "", "Environment to promote to (required)")
 	completion.FlagAnnotation(command, "env", "__hamctl_get_environments")
-	// errors are skipped here as the only case they can occour are if thee flag
-	// does not exist on the command.
-	//nolint:errcheck
+
+	command.Flags().StringVarP(&fromEnvironment, "from-env", "", "", "Environment to promote from")
+	completion.FlagAnnotation(command, "from-env", "__hamctl_get_environments")
+
 	command.MarkFlagRequired("env")
 	command.Flags().StringVarP(&namespace, "namespace", "n", "", "Namespace the service is deployed to (defaults to env)")
 	completion.FlagAnnotation(command, "namespace", "__hamctl_get_namespaces")
+
 	return command
 }

cmd/hamctl/command/release.go
@@ -1,12 +1,10 @@
 package command
 
 import (
-	"fmt"
-	"net/http"
-
+	"github.com/lunarway/release-manager/cmd/hamctl/command/actions"
 	"github.com/lunarway/release-manager/cmd/hamctl/command/completion"
-	"github.com/lunarway/release-manager/internal/git"
 	httpinternal "github.com/lunarway/release-manager/internal/http"
+	"github.com/lunarway/release-manager/internal/intent"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -24,37 +22,20 @@ Release latest artifact from branch 'master' of service 'product' into environme
 
   hamctl release --service product --env dev --branch master`,
 		RunE: func(c *cobra.Command, args []string) error {
-			if branch != "" && artifact != "" {
+			switch {
+			case branch != "" && artifact != "":
 				return errors.New("--branch and --artifact cannot both be specificed")
-			}
-			if branch == "" && artifact == "" {
+
+			case branch == "" && artifact == "":
 				return errors.New("--branch or --artifact is required")
-			}
-			committerName, committerEmail, err := git.CommitterDetails()
-			if err != nil {
-				return err
-			}
-			var resp httpinternal.ReleaseResponse
-			path, err := client.URL("release")
-			if err != nil {
-				return err
-			}
-			err = client.Do(http.MethodPost, path, httpinternal.ReleaseRequest{
-				Service:        *service,
-				Environment:    environment,
-				Branch:         branch,
-				ArtifactID:     artifact,
-				CommitterName:  committerName,
-				CommitterEmail: committerEmail,
-			}, &resp)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("Release of service: %s\n", *service)
-			if resp.Status != "" {
-				fmt.Printf("%s\n", resp.Status)
-			} else {
-				fmt.Printf("[✓] Release of %s to %s initialized\n", resp.Tag, resp.ToEnvironment)
+			case branch != "":
+				artifactID, err := actions.ArtifactIDFromBranch(client, *service, branch)
+				if err != nil {
+					return err
+				}
+				actions.ReleaseArtifactID(client, *service, environment, artifactID, intent.NewReleaseBranch(branch))
+			case artifact != "":
+				actions.ReleaseArtifactID(client, *service, environment, artifact, intent.NewReleaseArtifact())
 			}
 
 			return nil

cmd/server/command/start.go
@@ -209,9 +209,7 @@ func NewStart(startOptions *startOptions) *cobra.Command {
 				// to avoid this chicken and egg issue. It is not a real problem as the
 				// consumer is started later on and this we are sure this gets set, it
 				// just complicates the flow of the code.
-				PublishPromote:           nil,
 				PublishReleaseArtifactID: nil,
-				PublishReleaseBranch:     nil,
 				PublishRollback:          nil,
 				PublishNewArtifact:       nil,
 				// retries for comitting changes into config repo
@@ -287,14 +285,6 @@ func NewStart(startOptions *startOptions) *cobra.Command {
 			}
 
 			eventHandlers := map[string]func([]byte) error{
-				flow.PromoteEvent{}.Type(): func(d []byte) error {
-					var event flow.PromoteEvent
-					err := event.Unmarshal(d)
-					if err != nil {
-						return errors.WithMessage(err, "unmarshal event")
-					}
-					return flowSvc.ExecPromote(context.Background(), event)
-				},
 				flow.ReleaseArtifactIDEvent{}.Type(): func(d []byte) error {
 					var event flow.ReleaseArtifactIDEvent
 					err := event.Unmarshal(d)
@@ -302,14 +292,6 @@ func NewStart(startOptions *startOptions) *cobra.Command {
 						return errors.WithMessage(err, "unmarshal event")
 					}
 					return flowSvc.ExecReleaseArtifactID(context.Background(), event)
-				},
-				flow.ReleaseBranchEvent{}.Type(): func(d []byte) error {
-					var event flow.ReleaseBranchEvent
-					err := event.Unmarshal(d)
-					if err != nil {
-						return errors.WithMessage(err, "unmarshal event")
-					}
-					return flowSvc.ExecReleaseBranch(context.Background(), event)
 				},
 				flow.RollbackEvent{}.Type(): func(d []byte) error {
 					var event flow.RollbackEvent
@@ -347,13 +329,7 @@ func NewStart(startOptions *startOptions) *cobra.Command {
 				return errors.WithMessage(err, "setup broker")
 			}
 
-			flowSvc.PublishPromote = func(ctx context.Context, event flow.PromoteEvent) error {
-				return brokerImpl.Publish(ctx, &event)
-			}
 			flowSvc.PublishReleaseArtifactID = func(ctx context.Context, event flow.ReleaseArtifactIDEvent) error {
-				return brokerImpl.Publish(ctx, &event)
-			}
-			flowSvc.PublishReleaseBranch = func(ctx context.Context, event flow.ReleaseBranchEvent) error {
 				return brokerImpl.Publish(ctx, &event)
 			}
 			flowSvc.PublishRollback = func(ctx context.Context, event flow.RollbackEvent) error {

cmd/server/http/describe.go
@@ -31,6 +31,8 @@ func describe(payload *payload, flowSvc *flow.Service) http.HandlerFunc {
 			describeRelease(ctx, payload, flowSvc, p.Namespace(), p.Environment(), p.Service())(w, r)
 		case "artifact":
 			describeArtifact(ctx, payload, flowSvc, p.Service())(w, r)
+		case "latest-artifact":
+			describeLatestArtifacts(ctx, payload, flowSvc, p.Service())(w, r)
 		default:
 			log.WithContext(ctx).Errorf("describe path not found: %+v", p)
 			notFound(w)
@@ -165,6 +167,51 @@ func describeArtifact(ctx context.Context, payload *payload, flowSvc *flow.Servi
 		})
 		if err != nil {
 			logger.Errorf("http: describe artifact: service '%s': marshal response failed: %v", service, err)
+		}
+	}
+}
+
+func describeLatestArtifacts(ctx context.Context, payload *payload, flowSvc *flow.Service, service string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if emptyString(service) {
+			requiredFieldError(w, "service")
+			return
+		}
+		values := r.URL.Query()
+		branch := values.Get("branch")
+		if emptyString(branch) {
+			requiredFieldError(w, "branch")
+			return
+		}
+
+		logger := log.WithContext(ctx).WithFields("service", service, "branch", branch)
+		ctx := r.Context()
+		resp, err := flowSvc.DescribeLatestArtifact(ctx, service, branch)
+		if err != nil {
+			if ctx.Err() == context.Canceled {
+				logger.Infof("http: describe latest artifact: service '%s': request cancelled", service)
+				cancelled(w)
+				return
+			}
+			switch errorCause(err) {
+			case git.ErrArtifactNotFound:
+				httpinternal.Error(w, fmt.Sprintf("no artifacts available for service '%s' and branch '%s'.", service, branch), http.StatusBadRequest)
+				return
+			default:
+				logger.Errorf("http: describe latest artifact: service '%s' and branch '%s': failed: %v", service, branch, err)
+				unknownError(w)
+				return
+			}
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		err = payload.encodeResponse(ctx, w, httpinternal.DescribeArtifactResponse{
+			Service:   service,
+			Artifacts: []artifact.Spec{resp},
+		})
+		if err != nil {
+			logger.Errorf("http: describe latest artifact: service '%s' and branch '%s': marshal response failed: %v", service, branch, err)
 		}
 	}
 }

cmd/server/http/promote.go
@@ -1,113 +1,0 @@
-package http
-
-import (
-	"context"
-	"fmt"
-	"net/http"
-
-	"github.com/lunarway/release-manager/internal/artifact"
-	"github.com/lunarway/release-manager/internal/flow"
-	"github.com/lunarway/release-manager/internal/git"
-	httpinternal "github.com/lunarway/release-manager/internal/http"
-	"github.com/lunarway/release-manager/internal/log"
-)
-
-func promote(payload *payload, flowSvc *flow.Service) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var req httpinternal.PromoteRequest
-		ctx := r.Context()
-		logger := log.WithContext(ctx)
-		err := payload.decodeResponse(ctx, r.Body, &req)
-		if err != nil {
-			logger.Errorf("http: promote: decode request body failed: %v", err)
-			invalidBodyError(w)
-			return
-		}
-		// default namespace to environment if it's empty. For most devlopers this
-		// allows them to avoid setting the namespace flag for requests.
-		if emptyString(req.Namespace) {
-			req.Namespace = req.Environment
-		}
-
-		if !req.Validate(w) {
-			return
-		}
-
-		logger = logger.WithFields("service", req.Service, "namespace", req.Namespace, "req", req)
-		result, err := flowSvc.Promote(ctx, flow.Actor{
-			Name:  req.CommitterName,
-			Email: req.CommitterEmail,
-		}, req.Environment, req.Namespace, req.Service)
-
-		var statusString string
-		if err != nil {
-			if ctx.Err() == context.Canceled {
-				logger.Infof("http: promote: service '%s' environment '%s': promote cancelled", req.Service, req.Environment)
-				cancelled(w)
-				return
-			}
-			switch errorCause(err) {
-			case flow.ErrReleaseProhibited:
-				logger.Infof("http: promote: service '%s' environment '%s': promote rejected: branch prohibited in environment: %v", req.Service, req.Environment, err)
-				httpinternal.Error(w, fmt.Sprintf("artifact cannot be promoted to environment '%s' due to branch restriction policy", req.Environment), http.StatusBadRequest)
-				return
-			case flow.ErrNothingToRelease:
-				statusString = "Environment is already up-to-date"
-				logger.Infof("http: promote: service '%s' environment '%s': promote skipped: environment up to date: %v", req.Service, req.Environment, err)
-			case git.ErrBranchBehindOrigin:
-				logger.Infof("http: promote: service '%s' environment '%s': %v", req.Service, req.Environment, err)
-				httpinternal.Error(w, "could not promote right now. Please try again in a moment.", http.StatusServiceUnavailable)
-				return
-			case flow.ErrUnknownEnvironment:
-				logger.Infof("http: promote: service '%s' environment '%s': promote rejected: %v", req.Service, req.Environment, err)
-				httpinternal.Error(w, fmt.Sprintf("unknown environment: %s", req.Environment), http.StatusBadRequest)
-				return
-			case flow.ErrNamespaceNotAllowedByArtifact:
-				logger.Infof("http: promote: service '%s' environment '%s': promote rejected: %v", req.Service, req.Environment, err)
-				httpinternal.Error(w, "namespace not allowed by artifact", http.StatusBadRequest)
-				return
-			case artifact.ErrFileNotFound:
-				logger.Infof("http: promote: service '%s' environment '%s': promote rejected: %v", req.Service, req.Environment, err)
-				httpinternal.Error(w, fmt.Sprintf("artifact not found for service '%s'. Are you missing a namespace?", req.Service), http.StatusBadRequest)
-				return
-			case flow.ErrUnknownConfiguration:
-				logger.Infof("http: promote: service '%s' environment '%s': promote rejected: %v", req.Service, req.Environment, err)
-				httpinternal.Error(w, fmt.Sprintf("configuration for environment '%s' not found for service '%s'. Is the environment specified in 'shuttle.yaml'?", req.Environment, req.Service), http.StatusBadRequest)
-				return
-			default:
-				logger.Errorf("http: promote: service '%s' environment '%s': promote failed: %v", req.Service, req.Environment, err)
-				unknownError(w)
-				return
-			}
-		}
-
-		var fromEnvironment string
-		switch req.Environment {
-		case "dev":
-			fromEnvironment = "master"
-		case "staging":
-			fromEnvironment = "dev"
-		case "prod":
-			fromEnvironment = "staging"
-		default:
-			fromEnvironment = req.Environment
-		}
-
-		if result.OverwritingNamespace != "" {
-			statusString = fmt.Sprintf("Namespace '%s' did not match that of the artifact and was overwritten to '%s'", req.Namespace, result.OverwritingNamespace)
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-		err = payload.encodeResponse(ctx, w, httpinternal.PromoteResponse{
-			Service:         req.Service,
-			FromEnvironment: fromEnvironment,
-			ToEnvironment:   req.Environment,
-			Tag:             result.ReleaseID,
-			Status:          statusString,
-		})
-		if err != nil {
-			logger.Errorf("http: promote: service '%s' environment '%s': marshal response failed: %v", req.Service, req.Environment, err)
-		}
-	}
-}

commit efd127f631958aa9ab89f23a32bf4a15273cd087

Fix HTTP bug in ArtifactIDFromBranch lookup
Currently hamctl will use the ArtifactIDFromBranch action to look up what to
release when using the branch flag.

    hamctl release --service foo --branch bar --env baz

The action has a bug in the endpoint construction where it adds the branch name
twice. Once as a path segment and later as a query parameter. The server expects
only a query parameter. The bug was introduced way back in
1482e499a5f3fada4434e183fc0743145bb62019 (#195) but as the old HTTP server mux
was not that strict the bug wasn't discovered. With the change to using
gorilla/mux in bea4c3f975e07d6a129194137cedcdb3ce26f717 (#407) where paths needs
to match strictly things stopped working.

cmd/hamctl/command/actions/artifact_id.go
@@ -37,7 +37,7 @@ func ArtifactIDFromBranch(client *httpinternal.Client, service string, branch st
 	var describeResp httpinternal.DescribeArtifactResponse
 	params := url.Values{}
 	params.Add("branch", branch)
-	path, err := client.URLWithQuery(fmt.Sprintf("describe/latest-artifact/%s/%s", service, branch), params)
+	path, err := client.URLWithQuery(fmt.Sprintf("describe/latest-artifact/%s", service), params)
 	if err != nil {
 		return "", err
 	}

cmd/server/http/http.go
@@ -36,6 +36,7 @@ func NewServer(opts *Options, slackClient *slack.Client, flowSvc *flow.Service, 
 	}
 	m := mux.NewRouter()
 	m.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.WithContext(r.Context()).Infof("Unknown HTTP endpoint called: %s", r.URL.String())
 		notFound(w)
 	})
 