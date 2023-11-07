6c7cfff6ddda3d97bbd3617a86c2cbfdc41d0217
Hoeg <psh@lunar.app>
2023-11-07 06:43:51 +0100

    fixed auth tests

---
+++ cmd/server/http/http_test.go
@@ -0,0 +1,75 @@
+package http
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAuthenticate(t *testing.T) {
+	tt := []struct {
+		name          string
+		serverToken   string
+		authorization string
+		status        int
+	}{
+		{
+			name:          "empty authorization",
+			serverToken:   "token",
+			authorization: "",
+			status:        http.StatusUnauthorized,
+		},
+		{
+			name:          "whitespace token",
+			serverToken:   "token",
+			authorization: "  ",
+			status:        http.StatusUnauthorized,
+		},
+		{
+			name:          "non-bearer authorization",
+			serverToken:   "token",
+			authorization: "non-bearer-token",
+			status:        http.StatusUnauthorized,
+		},
+		{
+			name:          "empty bearer authorization",
+			serverToken:   "token",
+			authorization: "Bearer ",
+			status:        http.StatusUnauthorized,
+		},
+		{
+			name:          "whitespace bearer authorization",
+			serverToken:   "token",
+			authorization: "Bearer      ",
+			status:        http.StatusUnauthorized,
+		},
+		{
+			name:          "wrong bearer authorization",
+			serverToken:   "token",
+			authorization: "Bearer another-token",
+			status:        http.StatusUnauthorized,
+		},
+		{
+			name:          "correct bearer authorization",
+			serverToken:   "token",
+			authorization: "Bearer token",
+			status:        http.StatusOK,
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			verifier := Verifier{}
+			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+			})
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			req.Header.Set("Authorization", tc.authorization)
+			w := httptest.NewRecorder()
+			verifier.authentication(tc.serverToken)(handler).ServeHTTP(w, req)
+
+			assert.Equal(t, tc.status, w.Result().StatusCode, "status code not as expected")
+		})
+	}
+}
