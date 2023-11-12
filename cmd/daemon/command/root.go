d5ca5058da1ae0c521f6111c1208c77c7c4047d9
Bjørn Sørensen <bso@lunar.app>
2023-11-12 15:04:42 +0100

    Add log init in root command to support bad flags logging

--- cmd/daemon/command/root.go
+++ cmd/daemon/command/root.go
@@ -2,10 +2,16 @@ package command
 
 import (
 	"github.com/spf13/cobra"
+
+	"github.com/lunarway/release-manager/internal/log"
 )
 
 // NewRoot returns a new instance of a daemon command.
 func NewRoot(version string) (*cobra.Command, error) {
+	var logConfiguration *log.Configuration
+	logConfiguration.ParseFromEnvironmnet()
+	log.Init(logConfiguration)
+
 	var command = &cobra.Command{
 		Use:   "daemon",
 		Short: "daemon",
