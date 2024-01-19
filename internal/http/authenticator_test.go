4680e9ae5e7f23fd0588c1d74f1cd400fbd6ebee
Hoeg <psh@lunar.app>
2024-01-19 12:57:14 +0100

    login called on action

--- internal/http/authenticator_test.go
+++ internal/http/authenticator_test.go
@@ -2,21 +2,26 @@ package http
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"testing"
 
 	"golang.org/x/oauth2"
 )
 
-func TestAccess(t *testing.T) {
-	ua := UserAuthenticator{
-		conf: &HttpAuth{
-			DeviceResp: &oauth2.DeviceAuthResponse{
-				UserCode:                "YOLO",
-				VerificationURIComplete: "localhost",
-			},
+func TestAccessCallsLoginAndStoresToken(t *testing.T) {
+	httpAuth := HttpAuth{
+		DeviceResp: &oauth2.DeviceAuthResponse{
+			UserCode:                "YOLO",
+			VerificationURIComplete: "localhost",
 		},
-		ts:         &DummyStore{},
+	}
+	dummyStore := DummyStore{
+		token: nil,
+	}
+	ua := UserAuthenticator{
+		conf:       &httpAuth,
+		ts:         &dummyStore,
 		autoLogin:  true,
 		popBrowser: false,
 	}
@@ -28,10 +33,17 @@ func TestAccess(t *testing.T) {
 	if client == nil {
 		t.Fatal("client is nil")
 	}
+	if dummyStore.tokenRead == false {
+		t.Fatal("expected a read of the token")
+	}
+	if dummyStore.tokenStored == false {
+		t.Fatal("expected the token to be stored")
+	}
 }
 
 type HttpAuth struct {
 	DeviceResp *oauth2.DeviceAuthResponse
+	token      *oauth2.Token
 }
 
 func (ha *HttpAuth) DeviceAuth(ctx context.Context, opts ...oauth2.AuthCodeOption) (*oauth2.DeviceAuthResponse, error) {
@@ -39,7 +51,7 @@ func (ha *HttpAuth) DeviceAuth(ctx context.Context, opts ...oauth2.AuthCodeOptio
 }
 
 func (ha *HttpAuth) DeviceAccessToken(ctx context.Context, da *oauth2.DeviceAuthResponse, opts ...oauth2.AuthCodeOption) (*oauth2.Token, error) {
-	return nil, nil
+	return ha.token, nil
 }
 
 func (ha *HttpAuth) Client(ctx context.Context, t *oauth2.Token) *http.Client {
@@ -47,11 +59,19 @@ func (ha *HttpAuth) Client(ctx context.Context, t *oauth2.Token) *http.Client {
 }
 
 type DummyStore struct {
+	token       *oauth2.Token
+	tokenStored bool
+	tokenRead   bool
 }
 
 func (ds *DummyStore) storeAccessToken(token *oauth2.Token) error {
+	ds.tokenStored = true
 	return nil
 }
 func (ds *DummyStore) readAccessToken() (*oauth2.Token, error) {
-	return nil, nil
+	ds.tokenRead = true
+	if ds.token != nil {
+		return nil, fmt.Errorf("no token found")
+	}
+	return ds.token, nil
 }
